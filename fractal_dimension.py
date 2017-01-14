1454216006d85d3f01752a845b376ef8979fbaa7
Grant Sanderson <grantsanderson7@gmail.com>
2017-01-13 17:00:47 -0800

    Move tattoo project, and create fractal_dimension

---
+++ fractal_dimension.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from helpers import *
+
+from mobject.tex_mobject import TexMobject
+from mobject import Mobject
+from mobject.image_mobject import ImageMobject
+from mobject.vectorized_mobject import *
+
+from animation.animation import Animation
+from animation.transform import *
+from animation.simple_animations import *
+from animation.playground import *
+from topics.geometry import *
+from topics.characters import *
+from topics.functions import *
+from topics.fractals import *
+from topics.number_line import *
+from topics.combinatorics import *
+from topics.numerals import *
+from topics.three_dimensions import *
+from scene import Scene
+from camera import Camera
+from mobject.svg_mobject import *
+from mobject.tex_mobject import *
+
+
+class Test(Scene):
+    def construct(self):
+        pass
