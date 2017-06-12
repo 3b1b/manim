2fd7af3945739eed2b3a2a99d9d3cd07e649fad3
hhoffman14 <hannahoffman14@gmail.com>
2017-06-12 16:59:17 -0700

    finished change of basis, lin decomposition

--- less_messy/nice_vector.py
+++ less_messy/nice_vector.py
@@ -34,16 +34,30 @@ class NiceVector(Vector):
         """ changes the basis that self is expressed in,
             using the given change of base matrix.
         """
-        self.coords = np.dot(matrix,self.coords)
-        inverse = np.linalg.inv(matrix)
+        self.coords = np.dot(matrix,self.coords) #updates self.coords to new basis #(might have to transpose self.coords)
+        inverse = np.linalg.inv(matrix) #inverts the matrix
 
-        new_basis = np.array(self.dim)
+        new_basis = np.array(self.dim) 
         for i in range(self.dim):
             for j in range(self.dim):
-                new_basis[i][j] = inverse[i][j]*self.basis[j]
-        self.basis = new_basis
+                new_basis[i][j] = inverse[i][j]*self.basis[j] #reconstructs new basis from given matrix and old basis
+        self.basis = new_basis #resets basis
         
         return self
     
-    def show_as_lin_combo(self):
-        """ displays the 
+    def lin_combo(self):
+        """ returns a list of basis vectors
+            such that when the list is added up,
+            we get the vector
+        """
+        vector_list = []
+        for i in range(self.dim): #loop over dimensions
+            for j in range(abs(np.floor(self.coords[i]))): #whole basis vectors
+                vector_list += self.basis[i]
+            leftover = self.coords[i] - np.floor(self.coords[i]) #leftover, if coordinates are noninteger
+            if leftover != 0: #check if nonzero leftover
+                leftover_vec = []
+                for k in range(self.dim):
+                    leftover_vec += [leftover*self.coords[k]] #create scaled leftover vector
+                vector_list += leftover_vec #add leftover vector to overall vector list
+        return vector_list 
