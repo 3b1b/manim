c928bc425c1b2ac3063e4356d0d65a054f213f27
cclauss <cclauss@bluewin.ch>
2017-08-12 16:39:49 +0200

    import NumberLine, Point, Transform

--- old_projects/number_line_scene.py
+++ old_projects/number_line_scene.py
@@ -1,4 +1,7 @@
 import numpy as np
+from animation.transform import Transform
+from point_cloud_mobject import Point
+from topics.number_line import NumberLine
 
 class NumberLineScene(Scene):
     def construct(self, **number_line_config):
