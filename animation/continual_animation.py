a0fc3ce3241134e65e2adb56ecae4522edaa8f63
Sridhar Ramesh <sridhar@3blue1brown.com>
2018-01-10 17:56:14 -0800

    Bug fix on CycleAnimation

--- animation/continual_animation.py
+++ animation/continual_animation.py
@@ -128,12 +128,14 @@ class NormalAnimationAsContinualAnimation(ContinualAnimation):
         )
 
 class CycleAnimation(ContinualAnimation):
-    def __init__(self, *animation, **kwargs):
+    def __init__(self, animation, **kwargs):
         self.animation = animation
         ContinualAnimation.__init__(self, animation.mobject, **kwargs)
 
     def update_mobject(self, dt):
-        sef.animation.update(self.internal_time % self.animation.run_time)
+        mod_value = self.internal_time % self.animation.run_time
+        alpha = mod_value/float(self.animation.run_time)
+        self.animation.update(alpha)
 
 
 
