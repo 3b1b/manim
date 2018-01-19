7cbc83df20189e4a3a585b87ff76126658f435a8
Grant Sanderson <grantsanderson7@gmail.com>
2018-01-18 16:46:38 -0800

    Improved how functions are drawn

--- topics/functions.py
+++ topics/functions.py
@@ -4,50 +4,47 @@ from mobject.vectorized_mobject import VMobject
 
 from helpers import *
 
-class FunctionGraph(VMobject):
-    CONFIG = {
-        "color" : YELLOW,
-        "x_min" : -SPACE_WIDTH,
-        "x_max" : SPACE_WIDTH,
-        "num_steps" : 20,
-    }
-    def __init__(self, function, **kwargs):
-        self.function = function
-        VMobject.__init__(self, **kwargs)
-
-    def generate_points(self):
-        x_values = np.linspace(self.x_min, self.x_max, self.num_steps)
-        y_values = self.function(x_values)
-        okay_indices = np.isfinite(y_values)
-        x_values = x_values[okay_indices]
-        y_values = y_values[okay_indices]
-        self.set_anchor_points([
-            x*RIGHT + y*UP
-            for x, y in zip(x_values, y_values)
-        ], mode = "smooth")
-
-    def get_function(self):
-        return self.function
-
-
 class ParametricFunction(VMobject):
     CONFIG = {
         "t_min" : 0,
         "t_max" : 1,
-        "num_anchor_points" : 10,
+        "num_anchor_points" : 100,
     }
     def __init__(self, function, **kwargs):
         self.function = function
         VMobject.__init__(self, **kwargs)
 
     def generate_points(self):
-        t_values = np.linspace(
-            self.t_min, self.t_max, self.num_anchor_points
+        n_points = 3*self.num_anchor_points - 2
+        self.points = np.zeros((n_points, self.dim))
+        self.points[:,0] = np.linspace(
+            self.t_min, self.t_max, n_points
         )
-        points = np.array(map(self.function, t_values))
-        okay_indices = np.apply_along_axis(np.all, 1, np.isfinite(points))
-        points = points[okay_indices]
-        self.set_anchor_points(points, mode = "smooth")
+        #VMobject.apply_function takes care of preserving
+        #desirable tangent line properties at anchor points
+        self.apply_function(lambda p : self.function(p[0]))
+
+class FunctionGraph(ParametricFunction):
+    CONFIG = {
+        "color" : YELLOW,
+        "x_min" : -SPACE_WIDTH,
+        "x_max" : SPACE_WIDTH,
+    }
+    def __init__(self, function, **kwargs):
+        digest_config(self, kwargs)
+        parametric_function = lambda t : t*RIGHT + function(t)*UP
+        ParametricFunction.__init__(
+            self, 
+            parametric_function,
+            t_min = self.x_min, 
+            t_max = self.x_max,
+            **kwargs
+        )
+        self.function = function
+
+    def get_function(self):
+        return self.function
+
 
 
 
