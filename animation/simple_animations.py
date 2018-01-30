f0ed90a8845b4f55885029c15ea0d82523a220eb
Sridhar Ramesh <sridhar@3blue1brown.com>
2018-01-29 17:56:30 -0800

    Minor improvements to some details of Successor implementation

--- animation/simple_animations.py
+++ animation/simple_animations.py
@@ -431,19 +431,24 @@ class Succession(Animation):
         self.current_anim_index = 0 #TODO: What if self.num_anims == 0?
 
         self.mobject = Group()
-        self.jump_to_start_of_anim(0)
+        self.current_anim_index = 0
+        self.current_alpha = 0
+
         Animation.__init__(self, self.mobject, run_time = run_time, **kwargs)
 
+    # Beware: This does NOT take care of updating the subanimation to 0
+    # This was important to avoid a pernicious possibility in which subanimations were called
+    # with update(0) twice, which could in turn call a sub-Succession with update(0) four times,
+    # continuing exponentially
     def jump_to_start_of_anim(self, index):
+        if index != self.current_anim_index:
+            self.mobject.remove(*self.mobject.submobjects) # Should probably have a cleaner "remove_all" method...
+            self.mobject.add(self.animations[index].mobject)
+            for m in self.scene_mobjects_at_time[index].submobjects:
+                self.mobject.add(m)
+
         self.current_anim_index = index
         self.current_alpha = self.critical_alphas[index]
-
-        self.mobject.remove(*self.mobject.submobjects) # Should probably have a cleaner "remove_all" method...
-        self.mobject.add(self.animations[index].mobject)
-        for m in self.scene_mobjects_at_time[index].submobjects:
-            self.mobject.add(m)
-
-        self.animations[index].update(0)  
 
     def update_mobject(self, alpha):
         i = 0
