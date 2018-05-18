10c01db6e01c03bbf3276ddf3e84f00c078e4833
Grant Sanderson <grant@3blue1brown.com>
2018-05-18 16:54:30 -0700

    Replaced use of SmoothedVectorizedHomotopy with ComplexHomotopy in ComplexTransformationScene

--- once_useful_constructs/complex_transformation_scene.py
+++ once_useful_constructs/complex_transformation_scene.py
@@ -1,7 +1,8 @@
 from constants import *
 
 from animation.animation import Animation
-from animation.movement import SmoothedVectorizedHomotopy
+# from animation.movement import SmoothedVectorizedHomotopy
+from animation.movement import ComplexHomotopy
 from animation.transform import MoveToTarget
 from mobject.coordinate_systems import ComplexPlane
 from mobject.types.vectorized_mobject import VGroup
@@ -145,15 +146,12 @@ class ComplexTransformationScene(Scene):
     def apply_complex_homotopy(self, complex_homotopy, added_anims=[], **kwargs):
         transformer, transform_kwargs = self.get_transformer(**kwargs)
 
-        def homotopy(x, y, z, t):
-            output = complex_homotopy(complex(x, y), t)
-            rescaled_output = self.z_to_point(output)
-            return (rescaled_output.real, rescaled_output.imag, z)
+        # def homotopy(x, y, z, t):
+        #     output = complex_homotopy(complex(x, y), t)
+        #     rescaled_output = self.z_to_point(output)
+        #     return (rescaled_output.real, rescaled_output.imag, z)
 
         self.play(
-            SmoothedVectorizedHomotopy(
-                homotopy, transformer,
-                **transform_kwargs
-            ),
+            ComplexHomotopy(complex_homotopy, transformer, **transform_kwargs),
             *added_anims
         )
