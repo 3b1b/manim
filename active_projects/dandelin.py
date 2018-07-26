c4655fa31b1a1164f36f50667f3969b71e629df2
Grant Sanderson <grant@3blue1brown.com>
2018-07-25 23:57:49 -0700

    Set up scene stubs for 3d animations

---
+++ active_projects/dandelin.py
@@ -0,0 +1,66 @@
+from __future__ import absolute_import
+from big_ol_pile_of_manim_imports import *
+
+
+class SliceCone(ExternallyAnimatedScene):
+    pass
+
+
+class TiltPlane(ExternallyAnimatedScene):
+    pass
+
+
+class IntroduceConeEllipseFocalSum(ExternallyAnimatedScene):
+    pass
+
+
+class IntroduceSpheres(ExternallyAnimatedScene):
+    pass
+
+
+class ShowTangencyPoints(ExternallyAnimatedScene):
+    pass
+
+
+class ShowFocalLinesAsTangent(ExternallyAnimatedScene):
+    pass
+
+
+class RemindAboutTangencyToCone(ExternallyAnimatedScene):
+    pass
+
+
+class ShowCircleToCircleLine(ExternallyAnimatedScene):
+    pass
+
+
+class ShowCircleToCircleLineAtMultiplePoints(ExternallyAnimatedScene):
+    pass
+
+
+class ConjectureLineEquivalence(ExternallyAnimatedScene):
+    pass
+
+
+class ShowBigSphereTangentLines(ExternallyAnimatedScene):
+    pass
+
+
+class ShowFocalSumEqualsCircleDistance(ExternallyAnimatedScene):
+    pass
+
+
+class FinalMovingEllipsePoint(ExternallyAnimatedScene):
+    pass
+
+
+class TiltPlaneWithSpheres(ExternallyAnimatedScene):
+    pass
+
+
+class DandelinSpheresInCylinder(ExternallyAnimatedScene):
+    pass
+
+
+class CylinderDandelinSpheresChangingSlope(ExternallyAnimatedScene):
+    pass
