a229bdc9c939f7046a9ddd43efc99245c07bf9f4
Mehmet Mert Yildiran <mehmetmertyildiran@gmail.com>
2018-11-01 11:23:34 +0300

    Add a stream lock and an idle state for stream to provide continuous frame flow

--- scene/scene.py
+++ scene/scene.py
@@ -7,6 +7,11 @@ import random
 import shutil
 import subprocess as sp
 import warnings
+from time import sleep
+try:
+    import thread  # Low-level threading API (Python 2.7)
+except ImportError:
+    import _thread as thread  # Low-level threading API (Python 3.x)
 
 from tqdm import tqdm as ProgressDisplay
 
@@ -58,6 +63,7 @@ class Scene(Container):
         self.frame_num = 0
         self.current_scene_time = 0
         self.original_skipping_status = self.skip_animations
+        self.stream_lock = False
         if self.name is None:
             self.name = self.__class__.__name__
         if self.random_seed is not None:
@@ -455,6 +461,8 @@ class Scene(Container):
                 raise EndSceneEarlyException()
 
     def play(self, *args, **kwargs):
+        if IS_LIVE_STREAMING:
+            self.stream_lock = False
         if len(args) == 0:
             warnings.warn("Called Scene.play with no animations")
             return
@@ -491,7 +499,20 @@ class Scene(Container):
         else:
             self.continual_update(0)
         self.num_plays += 1
+
+        if IS_LIVE_STREAMING:
+            self.stream_lock = True
+            thread.start_new_thread(self.idle_stream, ())
+
         return self
+
+    def idle_stream(self):
+        while(self.stream_lock):
+            self.update_frame()
+            n_frames = 1
+            frame = self.get_frame()
+            self.add_frames(*[frame] * n_frames)
+            sleep(self.frame_duration * 999/1000)
 
     def clean_up_animations(self, *animations):
         for animation in animations:
