854ed3038e04f27f6661667a3c9a70dab550a274
lkevinzc <38581401+lkevinzc@users.noreply.github.com>
2018-12-27 19:12:52 +0800

    Update mobject.py

--- manimlib/mobject/mobject.py
+++ manimlib/mobject/mobject.py
@@ -530,7 +530,7 @@ class Mobject(Container):
     def add_background_rectangle(self, color=BLACK, opacity=0.75, **kwargs):
         # TODO, this does not behave well when the mobject has points,
         # since it gets displayed on top
-        from mobject.shape_matchers import BackgroundRectangle
+        from manimlib.mobject.shape_matchers import BackgroundRectangle
         self.background_rectangle = BackgroundRectangle(
             self, color=color,
             fill_opacity=opacity,
