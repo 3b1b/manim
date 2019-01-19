809dee6837b30474adf59d0bb725125778b6e7cc
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2019-01-18 20:33:13 -0600

    Update brace.py

--- manimlib/mobject/svg/brace.py
+++ manimlib/mobject/svg/brace.py
@@ -137,3 +137,137 @@ class BraceText(BraceLabel):
     CONFIG = {
         "label_constructor": TextMobject
     }
+    
+
+#############################
+class Bracket(TexMobject):
+    CONFIG = {
+        "buff": 0.2,
+        "width_multiplier": 2,
+        "max_num_quads": 15,
+        "min_num_quads": 0,
+        "background_stroke_width": 0,
+        "parametro": 2,
+    }
+
+    def __init__(self, mobject, direction=DOWN,**kwargs):
+        digest_config(self, kwargs, locals())
+        angle = -np.arctan2(*direction[:2]) + np.pi
+        mobject.rotate(-angle, about_point=ORIGIN)
+        left = mobject.get_corner(DOWN + LEFT)
+        right = mobject.get_corner(DOWN + RIGHT)
+        target_width = right[0] - left[0]
+
+        # Adding int(target_width) qquads gives approximately the right width
+        num_quads = np.clip(
+            int(self.width_multiplier * target_width*self.parametro),
+            self.min_num_quads, self.max_num_quads
+        )
+        tex_string = "\\underbracket{%s}" % (num_quads * "\\qquad")
+        TexMobject.__init__(self, tex_string, **kwargs)
+        self.tip_point_index = np.argmin(self.get_all_points()[:, 1])
+        self.stretch_to_fit_width(target_width)
+        self.shift(left - self.get_corner(UP + LEFT) + self.buff * DOWN)
+        for mob in mobject, self:
+            mob.rotate(angle, about_point=ORIGIN)
+
+    def put_at_tip(self, mob, use_next_to=True, **kwargs):
+        if use_next_to:
+            mob.next_to(
+                self.get_tip(),
+                np.round(self.get_direction()),
+                **kwargs
+            )
+        else:
+            mob.move_to(self.get_tip())
+            buff = kwargs.get("buff", DEFAULT_MOBJECT_TO_MOBJECT_BUFFER)
+            shift_distance = mob.get_width() / 2.0 + buff
+            mob.shift(self.get_direction() * shift_distance)
+        return self
+
+    def get_text(self, *text, **kwargs):
+        text_mob = TextMobject(*text)
+        self.put_at_tip(text_mob, **kwargs)
+        return text_mob
+
+    def get_tex(self, *tex, **kwargs):
+        tex_mob = TexMobject(*tex)
+        self.put_at_tip(tex_mob, **kwargs)
+        return tex_mob
+
+    def get_tip(self):
+        # Very specific to the LaTeX representation
+        # of a brace, but it's the only way I can think
+        # of to get the tip regardless of orientation.
+        return self.get_all_points()[self.tip_point_index]
+
+    def get_direction(self):
+        vect = self.get_tip() - self.get_center()
+        return vect / get_norm(vect)
+############################
+class Parentheses(TexMobject):
+    CONFIG = {
+        "buff": 0.2,
+        "width_multiplier": 2,
+        "max_num_quads": 15,
+        "min_num_quads": 0,
+        "background_stroke_width": 0,
+        "parametro": 2,
+    }
+
+    def __init__(self, mobject, direction=DOWN,**kwargs):
+        digest_config(self, kwargs, locals())
+        angle = -np.arctan2(*direction[:2]) + np.pi
+        mobject.rotate(-angle, about_point=ORIGIN)
+        left = mobject.get_corner(DOWN + LEFT)
+        right = mobject.get_corner(DOWN + RIGHT)
+        target_width = right[0] - left[0]
+
+        # Adding int(target_width) qquads gives approximately the right width
+        num_quads = np.clip(
+            int(self.width_multiplier * target_width*self.parametro),
+            self.min_num_quads, self.max_num_quads
+        )
+        tex_string = "\\aoverbrace[l@{}r]{%s}" % (num_quads * "\\qquad")
+        TexMobject.__init__(self, tex_string, **kwargs)
+        self.tip_point_index = np.argmin(self.get_all_points()[:, 1])
+        self.stretch_to_fit_width(target_width)
+        self.shift(left - self.get_corner(UP + LEFT) + self.buff * DOWN)
+        for mob in mobject, self:
+            mob.rotate(angle, about_point=ORIGIN)
+
+    def put_at_tip(self, mob, use_next_to=True, **kwargs):
+        if use_next_to:
+            mob.next_to(
+                self.get_tip(),
+                np.round(self.get_direction()),
+                **kwargs
+            )
+        else:
+            mob.move_to(self.get_tip())
+            buff = kwargs.get("buff", DEFAULT_MOBJECT_TO_MOBJECT_BUFFER)
+            shift_distance = mob.get_width() / 2.0 + buff
+            mob.shift(self.get_direction() * shift_distance)
+        return self
+
+    def get_text(self, *text, **kwargs):
+        text_mob = TextMobject(*text)
+        self.put_at_tip(text_mob, **kwargs)
+        return text_mob
+
+    def get_tex(self, *tex, **kwargs):
+        tex_mob = TexMobject(*tex)
+        self.put_at_tip(tex_mob, **kwargs)
+        return tex_mob
+
+    def get_tip(self):
+        # Very specific to the LaTeX representation
+        # of a brace, but it's the only way I can think
+        # of to get the tip regardless of orientation.
+        return self.get_all_points()[self.tip_point_index]
+
+    def get_direction(self):
+        vect = self.get_tip() - self.get_center()
+        return vect / get_norm(vect)
+############################
+
