39289dea7b0f79ff8fd32106f68484aaef1ac079
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2019-01-23 12:59:13 -0600

    Update music_scene.py

--- manimlib/scene/music_scene.py
+++ manimlib/scene/music_scene.py
@@ -2,6 +2,204 @@ from big_ol_pile_of_manim_imports import *
 
 class MusicalScene(Scene):
     CONFIG={
+        "prop": 1.32,
+        "pos": ORIGIN,
+        "grosor": 1,
+        "camera_config":{"background_color":WHITE},
+        "origen_do": ORIGIN,
+        "octavas": 4,
+        "color_soprano": RED,
+        "color_contra": BLUE,
+        "color_tenor": GREEN,
+        "color_bajo": PURPLE,
+    }
+    def t_b(self,prop):
+        return tecla_blanca().scale(prop).set_stroke(BLACK,self.grosor)
+    def t_n(self,prop):
+        return tecla_negra().scale(prop).set_stroke(BLACK,self.grosor)
+    def tec_ref(self,prop,pos,opac):
+        return self.t_b(prop).set_fill(WHITE, opacity = 0).move_to(pos)
+    def tec_rel(self,prop,ref,opac):
+        return self.t_b(prop).set_fill(WHITE, opacity = 0).next_to(ref,RIGHT,buff=0)
+    def tec_sharp(self,prop,ref,opac):
+        return self.t_n(prop).set_fill(BLACK, opacity = opac).move_to(ref.get_right()).shift(UP*(ref.get_height()-self.t_n(prop).get_height())/2)
+    def intervalo_p(self,n1,n2,direccion,**kwargs):
+        linea=Line(self.partitura[n1].get_center(),self.partitura[n2].get_center())
+        return Parentheses(linea,direccion,**kwargs)
+
+    def intervalo_b(self,n1,n2,direccion,**kwargs):
+        linea=Line(self.partitura[n1].get_center(),self.partitura[n2].get_center())
+        return Bracket(linea,direccion,**kwargs).set_stroke(None,0.1)
+
+    def intervalo_brace(self,n1,n2,direccion,**kwargs):
+        linea=Line(self.partitura[n1].get_center(),self.partitura[n2].get_center())
+        return Brace(linea,direccion,**kwargs).set_stroke(None,0.1)
+
+    def intervalo_v(self,n1,n2,texto,color_texto=BLACK,direccion=RIGHT,color_parentesis=TEAL,escala_texto=1,**kwargs):
+        parentesis=self.intervalo_p(n1,n2,direccion,buff=SMALL_BUFF*2,parametro=6,width=7).set_color(color_parentesis)
+        nombre=TexMobject("%s"%texto,color=color_texto).scale(escala_texto).next_to(parentesis,direccion,buff=SMALL_BUFF*1.5).add_background_rectangle(opacity=0.8,color=color_parentesis,buff=SMALL_BUFF)
+        return VGroup(parentesis,nombre)
+
+    def intervalo_h(self,n1,n2,texto,color_texto=BLACK,direccion=UP,color_flecha=ORANGE,escala_texto=0.7,buff_flecha=0.2,buff_texto=SMALL_BUFF*0.7,**kwargs):
+        flecha=Flecha(self.partitura[n1].get_right(),self.partitura[n2].get_left(),buff=buff_flecha).set_color(color_flecha)
+        nombre=TexMobject("%s"%texto,color=color_texto).scale(escala_texto).next_to(flecha,direccion,buff=buff_texto).add_background_rectangle(opacity=0.85,color=color_flecha,buff=SMALL_BUFF)
+        return VGroup(flecha,nombre)
+
+    def ap_inter_v(self,interv):
+        self.play(
+            GrowFromCenter(interv[0]),
+            GrowFromCenter(interv[1])
+            )
+
+    def ap_inter_h(self,interv):
+        self.play(GrowArrow(interv[0]),
+            GrowFromCenter(interv[1]))
+    def setup(self):
+        self.importar_partitura()
+        self.definir_cifrado()
+        self.definir_cambios_notas()
+
+        self.colores=[self.color_bajo,self.color_tenor,self.color_contra,self.color_soprano]
+        self.cambios_colores_teclas=[]
+        for t in range(len(self.teclas)):
+            self.cambios_colores_teclas.append(list(zip(self.teclas[t],self.colores)))
+
+        self.definir_colores()
+
+        for i_p,color in self.colores_notas:
+            for i in i_p:
+                self.partitura[i].set_color(color)
+
+
+
+    def definir_teclado(self,octavas,prop,opac,pos=ORIGIN):
+        teclado=VGroup()
+        for i in range(octavas):
+            if i==0:
+                teclado.add(self.tec_ref(prop,pos,opac))
+            else:
+                teclado.add(self.tec_rel(prop,teclado[-1],opac))
+            for tec in ["N","B","N","B","B0","N","B","N","B","N","B"]:
+                if tec=="N":
+                    teclado.add(self.tec_sharp(prop,teclado[-1],opac))
+                if tec=="B":
+                    teclado.add(self.tec_rel(prop,teclado[-2],opac))
+                if tec=="B0":
+                    teclado.add(self.tec_rel(prop,teclado[-1],opac))
+        return teclado
+
+    def definir_teclas(self,octavas):
+        sost=[]
+        tec_b=[]
+        for cont in range(octavas):
+            w=cont*12
+            sost=sost+[1+w,3+w,6+w,8+w,10+w]
+            tec_b=tec_b+[0+w,2+w,4+w,5+w,7+w,9+w,11+w]
+        return [sost,tec_b]
+
+    def definir_notas(self,octavas):
+        do=[*[12*n for n in range(octavas)]]
+        do_s=[*[1+12*n for n in range(octavas)]]
+        re=[*[2+12*n for n in range(octavas)]]
+        re_s=[*[3+12*n for n in range(octavas)]]
+        mi=[*[4+12*n for n in range(octavas)]]
+        fa=[*[5+12*n for n in range(octavas)]]
+        fa_s=[*[6+12*n for n in range(octavas)]]
+        sol=[*[7+12*n for n in range(octavas)]]
+        sol_s=[*[8+12*n for n in range(octavas)]]
+        la=[*[9+12*n for n in range(octavas)]]
+        la_s=[*[10+12*n for n in range(octavas)]]
+        si=[*[11+12*n for n in range(octavas)]]
+        return [do,do_s,re,re_s,mi,fa,fa_s,sol,sol_s,la,la_s,si]
+
+
+    def mandar_frente_sostenido(self,octavas,teclado):
+        for i in self.definir_teclas(octavas)[0]:
+            self.add_foreground_mobject(teclado[i])
+        for i in self.definir_teclas(octavas)[1]:
+            self.bring_to_back(teclado[i])
+
+    def importar_partitura(self):
+        pass
+
+    def definir_cambios_notas(self):
+        pass
+
+    def definir_colores(self):
+        pass
+
+
+    def definir_cifrado(self):
+        pass
+
+    def progresion(self,paso,simbolos_faltantes=[],**kwargs):
+        for pre_ind_n,post_ind_n in self.cambios_notas[paso]:
+            self.play(*[
+                ApplyMethod(
+                    self.teclado_transparente[i].set_fill,None,0
+                    )
+                for i,color in self.cambios_colores_teclas[paso]
+                ],
+                *[
+                ApplyMethod(
+                    self.teclado_transparente[i].set_fill,color,1
+                    )
+                for i,color in self.cambios_colores_teclas[paso+1]
+                ],
+                *[
+                ReplacementTransform(
+                    self.partitura[i].copy(),self.partitura[j],
+                    )
+                for i,j in zip(pre_ind_n,post_ind_n)
+                ],
+                Write(self.cifrado[paso+1]),
+                *[Write(self.partitura[w])for w in simbolos_faltantes],
+                **kwargs
+            )
+
+    def progresion_con_desfase(self,paso,desfase,x1=0,y1=0,x2=0,y2=0,r_n=0,simbolos_faltantes=[],**kwargs):
+        for pre_ind_n,post_ind_n in self.cambios_notas[r_n]:
+            self.play(*[
+                ApplyMethod(
+                    self.teclado_transparente[i].set_fill,None,0
+                    )
+                for i,color in self.cambios_colores_teclas[paso]
+                ],
+                *[
+                ApplyMethod(
+                    self.teclado_transparente[i].set_fill,color,1
+                    )
+                for i,color in self.cambios_colores_teclas[paso+1]
+                ],
+                *[
+                    ReplacementTransform(
+                        self.partitura[i].copy(),self.partitura[j],
+                        )
+                    for i,j in zip(range(desfase+x1,desfase+y1),range(desfase+x2,desfase+y2))
+                    ],
+                Write(self.cifrado[paso+1]),
+                *[Write(self.partitura[w])for w in simbolos_faltantes],
+                **kwargs
+            )   
+
+    def primer_paso(self,paso=0,simbolos_faltantes=[],run_time=0.5,**kwargs):
+        self.play(
+                *[
+                ApplyMethod(
+                    self.teclado_transparente[i].set_fill,color,1
+                    )
+                for i,color in self.cambios_colores_teclas[paso]
+                ],
+            Write(self.cifrado[paso]),
+            *[Write(self.partitura[w],run_time=2)for w in simbolos_faltantes],
+            run_time=run_time,
+            **kwargs
+        )
+
+from big_ol_pile_of_manim_imports import *
+
+class MusicalScene2(Scene):
+    CONFIG={
         "camera_config":{"background_color":WHITE},
         "grosor_linea_teclado": 1,
         "h_tb": 13.7,
