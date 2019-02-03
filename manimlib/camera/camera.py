593dd38b458c6641ad6052e5a002e3d2862fc331
Grant Sanderson <grant@3blue1brown.com>
2019-02-03 12:09:20 -0800

    frame_duration -> frame_rate

--- manimlib/camera/camera.py
+++ manimlib/camera/camera.py
@@ -30,7 +30,7 @@ class Camera(object):
         "background_image": None,
         "pixel_height": DEFAULT_PIXEL_HEIGHT,
         "pixel_width": DEFAULT_PIXEL_WIDTH,
-        "frame_duration": DEFAULT_FRAME_DURATION,
+        "frame_rate": DEFAULT_FRAME_RATE,
         # Note: frame height and width will be resized to match
         # the pixel aspect ratio
         "frame_height": FRAME_HEIGHT,
