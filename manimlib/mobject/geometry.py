2449f3a284297638b916615c27ee25f2a0f151b9
Grant Sanderson <grant@3blue1brown.com>
2019-02-07 21:59:32 -0800

    Remove Grid

--- manimlib/mobject/geometry.py
+++ manimlib/mobject/geometry.py
@@ -754,29 +754,3 @@ class RoundedRectangle(Rectangle):
     def __init__(self, **kwargs):
         Rectangle.__init__(self, **kwargs)
         self.round_corners(self.corner_radius)
-
-
-class Grid(VMobject):
-    CONFIG = {
-        "height": 6.0,
-        "width": 6.0,
-    }
-
-    def __init__(self, rows, columns, **kwargs):
-        digest_config(self, kwargs, locals())
-        VMobject.__init__(self, **kwargs)
-
-    def generate_points(self):
-        x_step = self.width / self.columns
-        y_step = self.height / self.rows
-
-        for x in np.arange(0, self.width + x_step, x_step):
-            self.add(Line(
-                [x - self.width / 2., -self.height / 2., 0],
-                [x - self.width / 2., self.height / 2., 0],
-            ))
-        for y in np.arange(0, self.height + y_step, y_step):
-            self.add(Line(
-                [-self.width / 2., y - self.height / 2., 0],
-                [self.width / 2., y - self.height / 2., 0]
-            ))
