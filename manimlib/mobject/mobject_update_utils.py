62c4967f210597b53a3ea84904d5849716226229
Grant Sanderson <grant@3blue1brown.com>
2019-02-11 13:04:52 -0800

    Add always function, for mobject updating by a method

--- manimlib/mobject/mobject_update_utils.py
+++ manimlib/mobject/mobject_update_utils.py
@@ -1,3 +1,9 @@
+def always(method, *args, **kwargs):
+    mobject = method.__self__
+    func = method.__func__
+    mobject.add_updater(lambda m: func(m, *args, **kwargs))
+
+
 def always_redraw(func):
     mob = func()
     mob.add_updater(lambda m: mob.become(func()))
