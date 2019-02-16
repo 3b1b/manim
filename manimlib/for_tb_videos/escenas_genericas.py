d4cf8e3e242140697140a7eddae70ae3db7e9f0f
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2019-02-16 12:44:36 -0600

    Update escenas_genericas.py

--- manimlib/for_tb_videos/escenas_genericas.py
+++ manimlib/for_tb_videos/escenas_genericas.py
@@ -44,7 +44,7 @@ class CheckSVG(Scene):
                 height=height_size
             ).rotate(self.angle).set_fill(self.fill_color,self.fill_opacity).scale(self.svg_scale)
         else:
-            self.imagen=self.import_text().set_fill(self.fill_color,self.fill_opacity).rotate(self.angle).set_stroke(self.stroke_color,self.stroke_width)
+            self.imagen=self.import_text().set_fill(self.fill_color,self.fill_opacity).rotate(self.angle).set_stroke(self.stroke_color,0)
             if self.set_size=="width":
                 self.imagen.set_width(FRAME_WIDTH)
             else:
@@ -101,6 +101,196 @@ class CheckSVG(Scene):
                     permission_print=False
             if permission_print:
                 self.add(element)
+            c = c + 1 
+
+    def return_elements(self,formula,adds):
+        for i in adds:
+            self.add_foreground_mobjects(formula[i].set_color(self.color_element),
+                TexMobject("%d"%i,color=self.color_element,background_stroke_width=0).scale(self.numbers_scale).next_to(formula[i],self.direction_numbers,buff=self.space_between_numbers))
+
+class CheckFormula(Scene):
+    CONFIG={
+    "camera_config":{"background_color": BLACK},
+    "svg_type":"text",
+    "file":"",
+    "svg_scale":0.9,
+    "angle":0,
+    "flip_svg":False,
+    "fill_opacity": 1,
+    "remove": [],
+    "stroke_color": WHITE,
+    "fill_color": WHITE,
+    "stroke_width": 0,
+    "numbers_scale":0.5,
+    "show_numbers": True,
+    "animation": False,
+    "direction_numbers": UP,
+    "color_numbers": RED,
+    "space_between_numbers":0,
+    "show_elements":[],
+    "color_element":BLUE,
+    "set_size":"width",
+    "remove_stroke":[],
+    "show_stroke":[],
+    "stroke_":1
+    }
+    def construct(self):
+        self.imagen=self.import_text()
+            if self.set_size=="width":
+                self.imagen.set_width(FRAME_WIDTH)
+            else:
+                self.imagen.set_height(FRAME_HEIGHT)
+            self.imagen.scale(self.svg_scale)
+        if self.flip_svg==True:
+            self.imagen.flip()
+        if self.show_numbers==True:
+            self.print_formula(self.imagen.copy(),
+                self.numbers_scale,
+                self.direction_numbers,
+                self.remove,
+                self.space_between_numbers,
+                self.color_numbers)
+
+        self.return_elements(self.imagen.copy(),self.show_elements)
+        for st in self.remove_stroke:
+            self.imagen[st].set_stroke(None,self.stroke_width)
+        for st in self.show_stroke:
+            self.imagen[st].set_stroke(None,self.stroke_)
+        if self.animation==True:
+            self.play(DrawBorderThenFill(self.imagen))
+        else:
+            self.add(self.imagen)
+        self.personalize_image()
+        self.wait()
+    def import_text(self):
+        return TexMobject("")
+
+    def personalize_image(self):
+        pass
+
+    def print_formula(self,text,inverse_scale,direction,exception,buff,color):
+        text.set_color(RED)
+        self.add(text)
+        c = 0
+        for j in range(len(text)):
+            permission_print=True
+            for w in exception:
+                if j==w:
+                    permission_print=False
+            if permission_print:
+                self.add(text[j].set_color(self.stroke_color))
+        c = c + 1
+
+        c=0
+        for j in range(len(text)):
+            permission_print=True
+            element = TexMobject("%d" %c,color=color)
+            element.scale(inverse_scale)
+            element.next_to(text[j],direction,buff=buff)
+            for w in exception:
+                if j==w:
+                    permission_print=False
+            if permission_print:
+                self.add_foreground_mobjects(element)
+            c = c + 1 
+
+    def return_elements(self,formula,adds):
+        for i in adds:
+            self.add_foreground_mobjects(formula[i].set_color(self.color_element),
+                TexMobject("%d"%i,color=self.color_element,background_stroke_width=0).scale(self.numbers_scale).next_to(formula[i],self.direction_numbers,buff=self.space_between_numbers))
+
+class CheckFormulaByTXT(Scene):
+    CONFIG={
+    "camera_config":{"background_color": BLACK},
+    "svg_type":"text",
+    "text": TexMobject(""),
+    "file":"",
+    "svg_scale":0.9,
+    "angle":0,
+    "flip_svg":False,
+    "fill_opacity": 1,
+    "remove": [],
+    "stroke_color": WHITE,
+    "fill_color": WHITE,
+    "stroke_width": 3,
+    "numbers_scale":0.5,
+    "show_numbers": True,
+    "animation": False,
+    "direction_numbers": UP,
+    "color_numbers": RED,
+    "space_between_numbers":0,
+    "show_elements":[],
+    "color_element":BLUE,
+    "set_size":"width",
+    "remove_stroke":[],
+    "show_stroke":[],
+    "warning_color":RED,
+    "stroke_":1
+    }
+    def construct(self):
+        self.imagen=self.text
+        if self.set_size=="width":
+            self.imagen.set_width(FRAME_WIDTH)
+        else:
+            self.imagen.set_height(FRAME_HEIGHT)
+        self.imagen.scale(self.svg_scale)
+        if self.flip_svg==True:
+            self.imagen.flip()
+        if self.show_numbers==True:
+            self.print_formula(self.imagen.copy(),
+                self.numbers_scale,
+                self.direction_numbers,
+                self.remove,
+                self.space_between_numbers,
+                self.color_numbers)
+
+        self.return_elements(self.imagen.copy(),self.show_elements)
+        for st in self.remove_stroke:
+            self.imagen[st].set_stroke(None,0)
+        for st in self.show_stroke:
+            self.imagen[st].set_stroke(None,self.stroke_)
+        if self.animation==True:
+            self.play(DrawBorderThenFill(self.imagen))
+        else:
+            c=0
+            for j in range(len(self.imagen)):
+                permission_print=True
+                for w in self.remove:
+                    if j==w:
+                        permission_print=False
+                if permission_print:
+                    self.add(self.imagen[j])
+            c = c + 1
+        self.personalize_image()
+        self.wait()
+
+    def personalize_image(self):
+        pass
+
+    def print_formula(self,text,inverse_scale,direction,exception,buff,color):
+        text.set_color(self.warning_color)
+        self.add(text)
+        c = 0
+        for j in range(len(text)):
+            permission_print=True
+            for w in exception:
+                if j==w:
+                    permission_print=False
+            if permission_print:
+                self.add(text[j].set_color(self.stroke_color))
+        c = c + 1
+
+        c=0
+        for j in range(len(text)):
+            permission_print=True
+            element = TexMobject("%d" %c,color=color)
+            element.scale(inverse_scale)
+            element.next_to(text[j],direction,buff=buff)
+            for w in exception:
+                if j==w:
+                    permission_print=False
+            if permission_print:
+                self.add_foreground_mobjects(element)
             c = c + 1 
 
     def return_elements(self,formula,adds):
