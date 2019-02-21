cc80f32ad75528d4091b53872d34f02378da84e3
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2019-02-21 05:18:07 -0600

    Update three_d_scene.py

--- manimlib/scene/three_d_scene.py
+++ manimlib/scene/three_d_scene.py
@@ -1,8 +1,7 @@
 from manimlib.animation.transform import ApplyMethod
 from manimlib.camera.three_d_camera import ThreeDCamera
 from manimlib.constants import DEGREES
-from manimlib.constants import PRODUCTION_QUALITY_FRAME_DURATION
-from manimlib.continual_animation.update import ContinualGrowValue
+from manimlib.constants import PRODUCTION_QUALITY_CAMERA_CONFIG
 from manimlib.mobject.coordinate_systems import ThreeDAxes
 from manimlib.mobject.geometry import Line
 from manimlib.mobject.three_dimensions import Sphere
@@ -10,7 +9,7 @@ from manimlib.mobject.types.vectorized_mobject import VGroup
 from manimlib.mobject.types.vectorized_mobject import VectorizedPoint
 from manimlib.scene.scene import Scene
 from manimlib.utils.config_ops import digest_config
-from manimlib.utils.config_ops import merge_config
+from manimlib.utils.config_ops import merge_dicts_recursively
 
 
 class ThreeDScene(Scene):
@@ -34,16 +33,15 @@ class ThreeDScene(Scene):
             self.camera.set_gamma(gamma)
 
     def begin_ambient_camera_rotation(self, rate=0.02):
-        self.ambient_camera_rotation = ContinualGrowValue(
-            self.camera.theta_tracker,
-            rate=rate
+        # TODO, use a ValueTracker for rate, so that it
+        # can begin and end smoothly
+        self.camera.theta_tracker.add_updater(
+            lambda m, dt: m.increment_value(rate * dt)
         )
-        self.add(self.ambient_camera_rotation)
+        self.add(self.camera.theta_tracker)
 
     def stop_ambient_camera_rotation(self):
-        if self.ambient_camera_rotation is not None:
-            self.remove(self.ambient_camera_rotation)
-        self.ambient_camera_rotation = None
+        self.camera.theta_tracker.clear_updaters()
 
     def move_camera(self,
                     phi=None,
@@ -146,20 +144,21 @@ class SpecialThreeDScene(ThreeDScene):
 
     def __init__(self, **kwargs):
         digest_config(self, kwargs)
-        if self.frame_duration == PRODUCTION_QUALITY_FRAME_DURATION:
+        if self.camera_config["pixel_width"] == PRODUCTION_QUALITY_CAMERA_CONFIG["pixel_width"]:
             config = {}
         else:
             config = self.low_quality_config
-        ThreeDScene.__init__(self, **merge_config([kwargs, config]))
+        config = merge_dicts_recursively(config, kwargs)
+        ThreeDScene.__init__(self, **config)
 
     def get_axes(self):
         axes = ThreeDAxes(**self.three_d_axes_config)
         for axis in axes:
             if self.cut_axes_at_radius:
-                p0 = axis.main_line.get_start()
+                p0 = axis.get_start()
                 p1 = axis.number_to_point(-1)
                 p2 = axis.number_to_point(1)
-                p3 = axis.main_line.get_end()
+                p3 = axis.get_end()
                 new_pieces = VGroup(
                     Line(p0, p1), Line(p1, p2), Line(p2, p3),
                 )
@@ -174,7 +173,7 @@ class SpecialThreeDScene(ThreeDScene):
         return axes
 
     def get_sphere(self, **kwargs):
-        config = merge_config([kwargs, self.sphere_config])
+        config = merge_dicts_recursively(self.sphere_config, kwargs)
         return Sphere(**config)
 
     def get_default_camera_position(self):
