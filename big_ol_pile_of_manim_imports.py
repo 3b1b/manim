8f73a652ab884753465772f8a4e1a58c38f73da3
Grant Sanderson <grant@3blue1brown.com>
2019-03-17 06:42:49 -0700

    Begin incorporating div_curl vector field functionality into the main library

--- big_ol_pile_of_manim_imports.py
+++ big_ol_pile_of_manim_imports.py
@@ -55,6 +55,7 @@ from manimlib.mobject.types.point_cloud_mobject import *
 from manimlib.mobject.types.vectorized_mobject import *
 from manimlib.mobject.mobject_update_utils import *
 from manimlib.mobject.value_tracker import *
+from manimlib.mobject.vector_field import *
 
 from manimlib.for_3b1b_videos.common_scenes import *
 from manimlib.for_3b1b_videos.pi_creature import *
