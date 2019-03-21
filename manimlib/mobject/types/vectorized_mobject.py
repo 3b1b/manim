84a950425cae19794d66df999e2b5b19f8b559d4
Grant Sanderson <grant@3blue1brown.com>
2019-03-21 09:42:06 -0700

    DashedVMobject should handle case when num_dashes=0

--- manimlib/mobject/types/vectorized_mobject.py
+++ manimlib/mobject/types/vectorized_mobject.py
@@ -918,22 +918,22 @@ class DashedVMobject(VMobject):
         VMobject.__init__(self, **kwargs)
         num_dashes = self.num_dashes
         ps_ratio = self.positive_space_ratio
+        if num_dashes > 0:
+            # End points of the unit interval for division
+            alphas = np.linspace(0, 1, num_dashes + 1)
 
-        # End points of the unit interval for division
-        alphas = np.linspace(0, 1, num_dashes + 1)
+            # This determines the length of each "dash"
+            full_d_alpha = (1.0 / num_dashes)
+            partial_d_alpha = full_d_alpha * ps_ratio
 
-        # This determines the length of each "dash"
-        full_d_alpha = (1.0 / num_dashes)
-        partial_d_alpha = full_d_alpha * ps_ratio
+            # Rescale so that the last point of vmobject will
+            # be the end of the last dash
+            alphas /= (1 - full_d_alpha + partial_d_alpha)
 
-        # Rescale so that the last point of vmobject will
-        # be the end of the last dash
-        alphas /= (1 - full_d_alpha + partial_d_alpha)
-
-        self.add(*[
-            vmobject.get_subcurve(alpha, alpha + partial_d_alpha)
-            for alpha in alphas[:-1]
-        ])
+            self.add(*[
+                vmobject.get_subcurve(alpha, alpha + partial_d_alpha)
+                for alpha in alphas[:-1]
+            ])
         # Family is already taken care of by get_subcurve
         # implementation
         self.match_style(vmobject, family=False)
