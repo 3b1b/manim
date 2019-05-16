aabd86488763458db867c8f6cffe129d32a73bf1
Alexander Vázquez <elteoremadebeethoven@gmail.com>
2019-05-15 19:12:55 -0500

    Update propaganda.py

--- proy_ext/propaganda.py
+++ proy_ext/propaganda.py
@@ -1190,3 +1190,269 @@ class Superficies(ThreeDScene):
 		#result.set_fill(RED_D, opacity=0.5)
 		#result.set_stroke(RED_E, width=0.5, opacity=0.5)
 		return result
+
+class Thumbnail(GraphScene):
+    CONFIG = {
+        "y_max": 8,
+        "y_axis_height": 5,
+        "default_riemann_start_color": PINK,
+        "default_riemann_end_color": PINK,
+    }
+
+    def construct(self):
+        self.add_title()
+        self.show_function_graph()
+        #self.otra_definicion()
+
+    def add_title(self):
+        title = self.title = TextMobject("\\sc Tutorial de Manim").scale(2.3)
+        title.to_edge(UP)
+        
+        h_line = Line(LEFT, RIGHT)
+        h_line.set_height(FRAME_WIDTH - 2 * LARGE_BUFF)
+        h_line.next_to(title, DOWN)
+        h_line.set_stroke(WHITE,3)
+        title.to_edge(UP+LEFT)
+        title.shift(RIGHT)
+        self.title=title
+        self.lin_h = h_line
+
+    def show_function_graph(self):
+        ul=underline(self.title)
+        self.setup_axes(animate=True,add_anims=[Escribe(self.title),ShowCreation(ul)])
+        self.add_foreground_mobjects(self.title,ul)
+        def func(x):
+            return 0.1 * (x + 3-5) * (x - 3-5) * (x-5) + 5
+
+        def rect(x):
+            return 2.775*(x-1.5)+3.862
+        recta = self.get_graph(rect,x_min=-1,x_max=5)
+        graph = self.get_graph(func,x_min=0.2,x_max=9)
+        graph.set_color(TT_AZUL_T)
+        input_tracker_p1 = ValueTracker(1.5)
+        input_tracker_p2 = ValueTracker(3.5)
+
+        def get_x_value_p1():
+            return input_tracker_p1.get_value()
+
+        def get_x_value_p2():
+            return input_tracker_p2.get_value()
+
+        def get_y_value_p1():
+            return graph.underlying_function(get_x_value_p1())
+
+        def get_y_value_p2():
+            return graph.underlying_function(get_x_value_p2())
+
+        def get_x_point_p1():
+            return self.coords_to_point(get_x_value_p1(), 0)
+
+        def get_x_point_p2():
+            return self.coords_to_point(get_x_value_p2(), 0)
+
+        def get_y_point_p1():
+            return self.coords_to_point(0, get_y_value_p1())
+
+        def get_y_point_p2():
+            return self.coords_to_point(0, get_y_value_p2())
+
+        def get_graph_point_p1():
+            return self.coords_to_point(get_x_value_p1(), get_y_value_p1())
+
+        def get_graph_point_p2():
+            return self.coords_to_point(get_x_value_p2(), get_y_value_p2())
+
+        def get_v_line_p1():
+            return DashedLine(get_x_point_p1(), get_graph_point_p1(), stroke_width=2)
+
+        def get_v_line_p2():
+            return DashedLine(get_x_point_p2(), get_graph_point_p2(), stroke_width=2)
+
+        def get_h_line_p1():
+            return DashedLine(get_graph_point_p1(), get_y_point_p1(), stroke_width=2)
+
+        def get_h_line_p2():
+            return DashedLine(get_graph_point_p2(), get_y_point_p2(), stroke_width=2)
+        # Triangulo a
+        input_triangle_p1 = RegularPolygon(n=3, start_angle=TAU / 4)
+        output_triangle_p1 = RegularPolygon(n=3, start_angle=0)
+        for triangle in input_triangle_p1, output_triangle_p1:
+            triangle.set_fill(WHITE, 1)
+            triangle.set_stroke(width=0)
+            triangle.scale(0.1)
+        # Triangulo b
+        input_triangle_p2 = RegularPolygon(n=3, start_angle=TAU / 4)
+        output_triangle_p2 = RegularPolygon(n=3, start_angle=0)
+        for triangle in input_triangle_p2, output_triangle_p2:
+            triangle.set_fill(WHITE, 1)
+            triangle.set_stroke(width=0)
+            triangle.scale(0.1)
+        # Entradas y salidas del triangulo a
+        input_triangle_update_p1 = ContinualUpdate(
+            input_triangle_p1, lambda m: m.move_to(get_x_point_p1(), DOWN)
+        )
+        output_triangle_update_p1 = ContinualUpdate(
+            output_triangle_p1, lambda m: m.move_to(get_y_point_p1(), LEFT)
+        )
+        # Entradas y salidas del triangulo b
+        input_triangle_update_p2 = ContinualUpdate(
+            input_triangle_p2, lambda m: m.move_to(get_x_point_p2(), DOWN)
+        )
+        output_triangle_update_p2 = ContinualUpdate(
+            output_triangle_p2, lambda m: m.move_to(get_y_point_p2(), LEFT)
+        )
+        # Etiqueas y salidas del punto a
+        x_label_p1 = TexMobject("a")
+        x_label_update_p1 = ContinualUpdate(
+            x_label_p1, lambda ma: ma.next_to(input_triangle_p1, DOWN, SMALL_BUFF)
+        )
+
+        output_label_p1 = TexMobject("f(a)")
+        output_label_update_p1 = ContinualUpdate(
+            output_label_p1, lambda ma: ma.next_to(
+                output_triangle_p1, LEFT, SMALL_BUFF)
+        )
+        # Etiqueas y salidas del punto b
+        x_label_p2 = TexMobject("b")
+        x_label_update_p2 = ContinualUpdate(
+            x_label_p2, lambda mb: mb.next_to(input_triangle_p2, DOWN, SMALL_BUFF)
+        )
+
+        output_label_p2 = TexMobject("f(b)")
+        output_label_update_p2 = ContinualUpdate(
+            output_label_p2, lambda mb: mb.next_to(
+                output_triangle_p2, LEFT, SMALL_BUFF)
+        )
+        # V_lines de a
+        v_line_p1 = get_v_line_p1()
+        v_line_update_p1 = ContinualUpdate(
+            v_line_p1, lambda vla: Transform(vla, get_v_line_p1()).update(1)
+        )
+        # V_lines de b
+        v_line_p2 = get_v_line_p2()
+        v_line_update_p2 = ContinualUpdate(
+            v_line_p2, lambda vlb: Transform(vlb, get_v_line_p2()).update(1)
+        )
+        # h_lines de a
+        h_line_p1 = get_h_line_p1()
+        h_line_update_p1 = ContinualUpdate(
+            h_line_p1, lambda hla: Transform(hla, get_h_line_p1()).update(1)
+        )
+        # h_lines de b
+        h_line_p2 = get_h_line_p2()
+        h_line_update_p2 = ContinualUpdate(
+            h_line_p2, lambda hlb: Transform(hlb, get_h_line_p2()).update(1)
+        )
+        # Animacion del punto a
+        graph_dot_p1 = Dot(color=TT_SIMBOLO)
+        graph_dot_update_p1 = ContinualUpdate(
+            graph_dot_p1, lambda ma: ma.move_to(get_graph_point_p1())
+        )
+        # Animacion del punto b
+        graph_dot_p2 = Dot(color=TT_SIMBOLO)
+        graph_dot_update_p2 = ContinualUpdate(
+            graph_dot_p2, lambda mb: mb.move_to(get_graph_point_p2())
+        )
+        #
+        self.play(
+            ShowCreation(graph),
+        )
+        # Animacion del punto a
+        self.add_foreground_mobject(graph_dot_p1)
+        self.add_foreground_mobject(graph_dot_p2)
+        self.play(
+            DrawBorderThenFill(input_triangle_p1),
+            Write(x_label_p1),
+            ShowCreation(v_line_p1),
+            GrowFromCenter(graph_dot_p1),
+            ShowCreation(h_line_p1),
+            Write(output_label_p1),
+            DrawBorderThenFill(output_triangle_p1),
+            DrawBorderThenFill(input_triangle_p2),
+            Write(x_label_p2),
+            ShowCreation(v_line_p2),
+            GrowFromCenter(graph_dot_p2),
+            ShowCreation(h_line_p2),
+            Write(output_label_p2),
+            DrawBorderThenFill(output_triangle_p2),
+            run_time=0.5
+        )
+        self.add(
+            input_triangle_update_p2,
+            x_label_update_p2,
+            graph_dot_update_p2,
+            v_line_update_p2,
+            h_line_update_p2,
+            output_triangle_update_p2,
+            output_label_update_p2,
+        )
+        ###################
+        pendiente_recta = self.get_secant_slope_group(
+            1.9, recta, dx = 1.4,
+            df_label = None,
+            dx_label = None,
+            dx_line_color = PURPLE,
+            df_line_color= ORANGE,
+            )
+        grupo_secante = self.get_secant_slope_group(
+            1.5, graph, dx = 2,
+            df_label = None,
+            dx_label = None,
+            dx_line_color = TT_FONDO_ROSA,
+            df_line_color= TT_FONDO_VERDE,
+            secant_line_color = RED,
+        )
+        start_dx = grupo_secante.kwargs["dx"]
+        start_x = grupo_secante.kwargs["x"]
+        def update_func_0(group, alpha):
+            dx = interpolate(start_dx, 4, alpha)
+            x = interpolate(start_x, 1.5, alpha)
+            kwargs = dict(grupo_secante.kwargs)
+            kwargs["dx"] = dx
+            kwargs["x"] = x
+            new_group = self.get_secant_slope_group(**kwargs)
+            Transform(group, new_group).update(1)
+            return group
+        def update_func_1(group, alpha):
+            dx = interpolate(start_dx, 0.001, alpha)
+            x = interpolate(start_x, 1.5, alpha)
+            kwargs = dict(grupo_secante.kwargs)
+            kwargs["dx"] = dx
+            kwargs["x"] = x
+            new_group = self.get_secant_slope_group(**kwargs)
+            Transform(group, new_group).update(1)
+            return group
+
+        self.add(
+            input_triangle_update_p2,
+            graph_dot_update_p2,
+            v_line_update_p2,
+            h_line_update_p2,
+            output_triangle_update_p2,
+        )
+        self.play(FadeIn(grupo_secante))
+
+        kwargs = {
+            "x_min" : 4,
+            "x_max" : 9,
+            "fill_opacity" : 0.75,
+            "stroke_width" : 0.25,
+            #"start_color":PURPLE,
+            #"end_color":ORANGE,
+        }
+        self.graph=graph
+        iteraciones=6
+
+
+        self.rect_list = self.get_riemann_rectangles_list(
+            graph, iteraciones,start_color=PURPLE,end_color=ORANGE, **kwargs
+        )
+        flat_rects = self.get_riemann_rectangles(
+            self.get_graph(lambda x : 0), dx = 0.5,start_color=invert_color(PURPLE),end_color=invert_color(ORANGE),**kwargs
+        )
+        rects = self.rect_list[0]
+        self.transform_between_riemann_rects(
+            flat_rects, rects, 
+            replace_mobject_with_target_in_scene = True,
+            run_time=0.9
+        )
