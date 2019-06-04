56610a1a88a6ee6848381059a488a7278b0ba3e0
zavden <davz95@hotmail.com>
2019-06-04 08:31:14 -0500

    Prueba 6

--- example_scenes.py
+++ example_scenes.py
@@ -494,8 +494,8 @@ class WarpSquare(Scene):
 class WriteStuff(Scene):
     def construct(self):
         example_text = TextMobject(
-            "Alexander is a some text",color=WHITE,
-            tex_to_color_map={"text": YELLOW,"some":N_ROJO_1}
+            "Alexander, cómo estas?? is a some text",color=WHITE,
+            tex_to_coloó_map={"text": YELLOW,"some":N_ROJO_1}
         )
         example_tex = TexMobject(
             "\\sum_{k=1}^\\infty {1 \\over k^2} = {\\pi^2 \\over 6}",
