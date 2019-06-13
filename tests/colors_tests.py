c466e5041af30b3160ee461e79ed0da367b60232
zavden <davz95@hotmail.com>
2019-06-13 01:44:08 -0500

    Pequeñas adiciones

--- tests/colors_tests.py
+++ tests/colors_tests.py
@@ -1,7 +1,11 @@
 from manimlib.imports import *
 
+set_custom_quality(1200,10)
+
+OUTPUT_DIRECTORY = "TESTS/COLOR_TESTS"
+
 class ColorScene1(Scene):
     def construct(self):
-        rectangle=Rectangle(fill_color=RED,stroke_color=RED,fill_opacity=1)
-        print(RED)
+        rectangle=Rectangle(fill_color=BLACK,stroke_color=BLACK,fill_opacity=1)
+        rectangle.scale(2)
         self.add(rectangle)
