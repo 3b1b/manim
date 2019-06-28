4104c1cda8c66905f73512fa3eb55e163f1b4afe
Yoshi Askharoun <jjask7@gmail.com>
2019-06-28 10:25:57 -0700

    Add addon_info as a valid command
    ``manim addon_info`` now returns a list of loaded addons and their details

--- manimlib/__init__.py
+++ manimlib/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys, subprocess
 import manimlib.config
 import manimlib.constants
 import manimlib.extract_scene
@@ -7,18 +8,23 @@ import manimlib.addon_helper
 
 
 def main():
-    print("Starting...", end="\r")
-
-    addon_helper.read_addons(True)
-    args = manimlib.config.parse_cli()
-    
-    if not args.livestream:
-        config = manimlib.config.get_configuration(args)
-        manimlib.constants.initialize_directories(config)
-        addon_helper.pass_config_to_addons(config)
-        manimlib.extract_scene.main(config)
+    if sys.argv[1] == "addon_info":
+        subprocess.run([
+            "python", "addon_info.py"
+        ])
     else:
-        manimlib.stream_starter.start_livestream(
-            to_twitch=args.to_twitch,
-            twitch_key=args.twitch_key,
-        )
+        print("Starting...", end="\r")
+
+        addon_helper.read_addons(True)
+        args = manimlib.config.parse_cli()
+        
+        if not args.livestream:
+            config = manimlib.config.get_configuration(args)
+            manimlib.constants.initialize_directories(config)
+            addon_helper.pass_config_to_addons(config)
+            manimlib.extract_scene.main(config)
+        else:
+            manimlib.stream_starter.start_livestream(
+                to_twitch=args.to_twitch,
+                twitch_key=args.twitch_key,
+            )
