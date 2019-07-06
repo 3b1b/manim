7f2f8b8281a3c670c65eb8adfda39bee45dda29a
Yoshi Askharoun <jjask7@gmail.com>
2019-07-06 14:16:59 -0700

    Fixed __init__.py
    Added ``manimlib.`` to every reference to ``addon_helper`` (even though the commands worked fine without it, the Travis CI build fails)

--- manimlib/__init__.py
+++ manimlib/__init__.py
@@ -15,13 +15,13 @@ def main():
     else:
         print("Starting...", end="\r")
 
-        addon_helper.read_addons(True)
+        manimlib.addon_helper.read_addons(True)
         args = manimlib.config.parse_cli()
         
         if not args.livestream:
             config = manimlib.config.get_configuration(args)
             manimlib.constants.initialize_directories(config)
-            addon_helper.pass_config_to_addons(config)
+            manimlib.addon_helper.pass_config_to_addons(config)
             manimlib.extract_scene.main(config)
         else:
             manimlib.stream_starter.start_livestream(
