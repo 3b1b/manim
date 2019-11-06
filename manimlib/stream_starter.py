bdbd214d3273c5a8c497bbc9311238630db584c4
Saswata Gupta <gupta.saswata@gmail.com>
2019-11-05 22:33:51 -0500

    Fixed livestream config to work with scene file writer

--- manimlib/stream_starter.py
+++ manimlib/stream_starter.py
@@ -7,35 +7,36 @@ import subprocess
 from manimlib.scene.scene import Scene
 import manimlib.constants
 
-
 def start_livestream(to_twitch=False, twitch_key=None):
     class Manim():
-
-        def __new__(cls):
-            kwargs = {
-                "scene_name": manimlib.constants.LIVE_STREAM_NAME,
-                "open_video_upon_completion": False,
-                "show_file_in_finder": False,
-                # By default, write to file
-                "write_to_movie": True,
-                "show_last_frame": False,
+        STREAMING_CONFIG = {
+            "scene_name": manimlib.constants.LIVE_STREAM_NAME,
+            "open_video_upon_completion": False,
+            "show_file_in_finder": False,
+            # By default, write to file
+            "file_writer_config": {
+                "write_to_movie": False,
                 "save_pngs": False,
-                # If -t is passed in (for transparent), this will be RGBA
-                "saved_image_mode": "RGB",
                 "movie_file_extension": ".mp4",
-                "quiet": True,
-                "ignore_waits": False,
-                "write_all": False,
-                "name": manimlib.constants.LIVE_STREAM_NAME,
-                "start_at_animation_number": 0,
-                "end_at_animation_number": None,
-                "skip_animations": False,
-                "camera_config": manimlib.constants.HIGH_QUALITY_CAMERA_CONFIG,
-                "livestreaming": True,
                 "to_twitch": to_twitch,
                 "twitch_key": twitch_key,
-            }
-            return Scene(**kwargs)
+                "livestreaming": True,
+            },
+            "show_last_frame": False,
+            # If -t is passed in (for transparent), this will be RGBA
+            "saved_image_mode": "RGB",
+            "quiet": True,
+            "ignore_waits": False,
+            "write_all": False,
+            "name": manimlib.constants.LIVE_STREAM_NAME,
+            "start_at_animation_number": 0,
+            "end_at_animation_number": None,
+            "skip_animations": False,
+            "camera_config": manimlib.constants.HIGH_QUALITY_CAMERA_CONFIG,
+        }
+        def __new__(cls):
+            manimlib.constants.initialize_directories(cls.STREAMING_CONFIG)
+            return Scene(**cls.STREAMING_CONFIG)
 
     if not to_twitch:
         FNULL = open(os.devnull, 'w')
