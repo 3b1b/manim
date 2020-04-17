87d8671995b92fc7ead3f1fc0e557b2ae15102b1
Devin Neal <devin@eulertour.com>
2020-04-16 23:59:22 -0700

    Remove livestreaming option

--- manim.py
+++ manim.py
@@ -3,5 +3,3 @@ import manimlib
 
 if __name__ == "__main__":
     manimlib.main()
-else:
-    manimlib.stream_starter.start_livestream()
--- manimlib/__init__.py
+++ manimlib/__init__.py
@@ -2,17 +2,10 @@
 import manimlib.config
 import manimlib.constants
 import manimlib.extract_scene
-import manimlib.stream_starter
 
 
 def main():
     args = manimlib.config.parse_cli()
-    if not args.livestream:
-        config = manimlib.config.get_configuration(args)
-        manimlib.constants.initialize_directories(config)
-        manimlib.extract_scene.main(config)
-    else:
-        manimlib.stream_starter.start_livestream(
-            to_twitch=args.to_twitch,
-            twitch_key=args.twitch_key,
-        )
+    config = manimlib.config.get_configuration(args)
+    manimlib.constants.initialize_directories(config)
+    manimlib.extract_scene.main(config)
