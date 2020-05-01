516fa0594637e02dcbdaccea950e45720e90db75
NavpreetDevpuri <30471072+NavpreetDevpuri@users.noreply.github.com>
2020-05-01 10:46:51 +0530

    Update text_mobject.py

--- manimlib/mobject/svg/text_mobject.py
+++ manimlib/mobject/svg/text_mobject.py
@@ -1,3 +1,7 @@
+import operator as op
+from manimlib.mobject.types.vectorized_mobject import VGroup
+from manimlib.mobject.types.vectorized_mobject import VectorizedPoint
+from manimlib.utils.strings import split_string_list_to_isolate_substrings
 import re
 import os
 import copy
@@ -19,16 +23,11 @@ class TextSetting(object):
         self.line_num = line_num
 
 
-class Text(SVGMobject):
+class SingleStringTextMobject(SVGMobject):
     CONFIG = {
         # Mobject
         'color': consts.WHITE,
         'height': None,
-        'width': None,
-        'fill_opacity': 1,
-        'stroke_width': 0,
-        "should_center": True,
-        "unpack_groups": True,
         # Text
         'font': '',
         'gradient': None,
@@ -41,6 +40,7 @@ class Text(SVGMobject):
         't2g': {},
         't2s': {},
         't2w': {},
+        "organize_left_to_right": False,
     }
 
     def __init__(self, text, **config):
@@ -50,6 +50,7 @@ class Text(SVGMobject):
         self.lsh = self.size if self.lsh == -1 else self.lsh
 
         file_name = self.text2svg()
+        self.replace_space_char(file_name)
         SVGMobject.__init__(self, file_name, **config)
 
         if self.t2c:
@@ -123,7 +124,7 @@ class Text(SVGMobject):
         settings = self.font + self.slant + self.weight
         settings += str(self.t2f) + str(self.t2s) + str(self.t2w)
         settings += str(self.lsh) + str(self.size)
-        id_str = self.text+settings
+        id_str = self.text + settings
         hasher = hashlib.sha256()
         hasher.update(id_str.encode())
         return hasher.hexdigest()[:16]
@@ -179,12 +180,12 @@ class Text(SVGMobject):
         size = self.size * 10
         lsh = self.lsh * 10
 
-        if self.font == '':
-            print(NOT_SETTING_FONT_MSG)
+        # if self.font == '':
+        # print(NOT_SETTING_FONT_MSG)
 
         dir_name = consts.TEXT_DIR
         hash_name = self.text2hash()
-        file_name = os.path.join(dir_name, hash_name)+'.svg'
+        file_name = os.path.join(dir_name, hash_name) + '.svg'
         if os.path.exists(file_name):
             return file_name
 
@@ -206,8 +207,180 @@ class Text(SVGMobject):
             if setting.line_num != last_line_num:
                 offset_x = 0
                 last_line_num = setting.line_num
-            context.move_to(START_X + offset_x, START_Y + lsh*setting.line_num)
+            context.move_to(START_X + offset_x, START_Y + lsh * setting.line_num)
             context.show_text(text)
             offset_x += context.text_extents(text)[4]
 
         return file_name
+
+    def replace_space_char(self, file_name):
+        file = open(file_name, "r")
+        xmltext = file.read()
+        file.close()
+        xmltext = xmltext.replace("d=\"\"", "d=\"" + self.get_space_path_str() + "\"")
+        file = open(file_name, "w")
+        file.write(xmltext)
+        file.close()
+
+    def get_space_path_str(self):
+        self.create_space_file()
+        dir_name = consts.TEXT_DIR
+        hash_name = "space"
+        file_name = os.path.join(dir_name, hash_name) + '.svg'
+        file = open(file_name, "r")
+        xmlstr = file.read()
+        temp = xmlstr.split("<path ")[1]
+        i = temp.find("d=\"")
+        j = temp.find("/>")
+        pathstr = temp[i + 3:j - 1]
+        temp = pathstr.split("L ")
+        x1 = float(temp[1].split(" ")[0])
+        x2 = float(temp[2].split(" ")[0])
+        empty_area_path_str = "M " + str(x1) + " 0 L " + str(x2) + " 0 Z M " + str(x1) + " 0 "
+        return empty_area_path_str
+
+    def create_space_file(self):
+        # anti-aliasing
+        size = self.size * 10
+        lsh = self.lsh * 10
+
+        dir_name = consts.TEXT_DIR
+        hash_name = "space"
+        file_name = os.path.join(dir_name, hash_name) + '.svg'
+
+        surface = cairo.SVGSurface(file_name, 600, 400)
+        context = cairo.Context(surface)
+        context.set_font_size(size)
+        context.move_to(START_X, START_Y)
+
+        settings = self.text2settings()
+        offset_x = 0
+        last_line_num = 0
+        for setting in settings:
+            font = setting.font
+            slant = self.str2slant(setting.slant)
+            weight = self.str2weight(setting.weight)
+            text = "_"
+
+            context.select_font_face(font, slant, weight)
+            if setting.line_num != last_line_num:
+                offset_x = 0
+                last_line_num = setting.line_num
+            context.move_to(START_X + offset_x, START_Y + lsh * setting.line_num)
+            context.show_text(text)
+            offset_x += context.text_extents(text)[4]
+
+
+class Text(SingleStringTextMobject):
+    CONFIG = {
+        "substrings_to_isolate": [],
+        "tex_to_color_map": {},
+        "template_tex_file_body": TEMPLATE_TEXT_FILE_BODY,
+        "alignment": "\\centering",
+        "arg_separator": "",
+
+    }
+
+    def __init__(self, *tex_strings, **kwargs):
+        digest_config(self, kwargs)
+        tex_strings = self.break_up_tex_strings(tex_strings)
+        self.tex_strings = tex_strings
+        SingleStringTextMobject.__init__(
+            self, self.arg_separator.join(tex_strings), **kwargs
+        )
+        self.break_up_by_substrings()
+        self.set_color_by_tex_to_color_map(self.tex_to_color_map)
+
+        if self.organize_left_to_right:
+            self.organize_submobjects_left_to_right()
+
+    def break_up_tex_strings(self, tex_strings):
+        substrings_to_isolate = op.add(
+            self.substrings_to_isolate,
+            list(self.tex_to_color_map.keys())
+        )
+        split_list = split_string_list_to_isolate_substrings(
+            tex_strings, *substrings_to_isolate
+        )
+        if self.arg_separator == ' ':
+            split_list = [str(x).strip() for x in split_list]
+        # split_list = list(map(str.strip, split_list))
+        split_list = [s for s in split_list if s != '']
+        return split_list
+
+    def break_up_by_substrings(self):
+        """
+        Reorganize existing submojects one layer
+        deeper based on the structure of tex_strings (as a list
+        of tex_strings)
+        """
+        new_submobjects = []
+        curr_index = 0
+        config = dict(self.CONFIG)
+        config["alignment"] = ""
+        for tex_string in self.tex_strings:
+            sub_tex_mob = SingleStringTextMobject(tex_string, **config)
+            num_submobs = len(sub_tex_mob.submobjects)
+            new_index = curr_index + num_submobs
+            if num_submobs == 0:
+                # For cases like empty tex_strings, we want the corresponing
+                # part of the whole TexMobject to be a VectorizedPoint
+                # positioned in the right part of the TexMobject
+                sub_tex_mob.submobjects = [VectorizedPoint()]
+                last_submob_index = min(curr_index, len(self.submobjects) - 1)
+                sub_tex_mob.move_to(self.submobjects[last_submob_index], RIGHT)
+            else:
+                sub_tex_mob.submobjects = self.submobjects[curr_index:new_index]
+            new_submobjects.append(sub_tex_mob)
+            curr_index = new_index
+        self.submobjects = new_submobjects
+        return self
+
+    def get_parts_by_tex(self, tex, substring=True, case_sensitive=True):
+        def test(tex1, tex2):
+            if not case_sensitive:
+                tex1 = tex1.lower()
+                tex2 = tex2.lower()
+            if substring:
+                return tex1 in tex2
+            else:
+                return tex1 == tex2
+
+        return VGroup(*[m for m in self.submobjects if test(tex, m.get_tex_string())])
+
+    def get_part_by_tex(self, tex, **kwargs):
+        all_parts = self.get_parts_by_tex(tex, **kwargs)
+        return all_parts[0] if all_parts else None
+
+    def set_color_by_tex(self, tex, color, **kwargs):
+        parts_to_color = self.get_parts_by_tex(tex, **kwargs)
+        for part in parts_to_color:
+            part.set_color(color)
+        return self
+
+    def set_color_by_tex_to_color_map(self, texs_to_color_map, **kwargs):
+        for texs, color in list(texs_to_color_map.items()):
+            try:
+                # If the given key behaves like tex_strings
+                texs + ''
+                self.set_color_by_tex(texs, color, **kwargs)
+            except TypeError:
+                # If the given key is a tuple
+                for tex in texs:
+                    self.set_color_by_tex(tex, color, **kwargs)
+        return self
+
+    def index_of_part(self, part):
+        split_self = self.split()
+        if part not in split_self:
+            raise Exception("Trying to get index of part not in TexMobject")
+        return split_self.index(part)
+
+    def index_of_part_by_tex(self, tex, **kwargs):
+        part = self.get_part_by_tex(tex, **kwargs)
+        return self.index_of_part(part)
+
+    def sort_alphabetically(self):
+        self.submobjects.sort(
+            key=lambda m: m.get_tex_string()
+        )
