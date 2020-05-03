142fe5ff88014dd1a463ed8f1c42bf5ba509f303
NavpreetDevpuri <30471072+NavpreetDevpuri@users.noreply.github.com>
2020-05-03 22:55:03 +0530

    Update code_mobject.py

--- manimlib/mobject/code_mobject.py
+++ manimlib/mobject/code_mobject.py
@@ -61,15 +61,15 @@ class CodeMobject(VGroup):
         self.lines = []
         for line_no in range(0, self.code_json.__len__()):
             line_str = ""
-            for j in range(self.code_json[line_no].__len__()):
-                line_str = line_str + self.code_json[line_no][j][0]
+            for word_index in range(self.code_json[line_no].__len__()):
+                line_str = line_str + self.code_json[line_no][word_index][0]
             line = Text(line_str,font=self.font, stroke_width=self.stroke_width).scale(self.scale_factor)
-            m = 0
-            for j in range(self.code_json[line_no].__len__()):
-                for k in range(self.code_json[line_no][j][0].__len__()):
-                    if self.code_json[line_no][j][0][k] != " ":
-                        line[m].set_color(self.code_json[line_no][j][1])
-                        m = m +1
+            line_char_index = 0
+            for word_index in range(self.code_json[line_no].__len__()):
+                for char_index in range(self.code_json[line_no][word_index][0].__len__()):
+                    if self.code_json[line_no][word_index][0][char_index] != " ":
+                        line[line_char_index].set_color(self.code_json[line_no][word_index][1])
+                        line_char_index = line_char_index +1
             if line_no == 0:
                 line.move_to(
                     np.array([self.coordinates[0], self.coordinates[1], 0]) +
@@ -87,68 +87,69 @@ class CodeMobject(VGroup):
     def gen_code_json(self):
         file = open(self.file_path, "r")
         html_string = file.read()
+        for i in range(10, -1, -1):
+            html_string = html_string.replace("</" + " " * i,"</")
         file.close()
-        i = html_string.find("&#xa0;")
-        str1 = html_string[0:i]
-        str1 = str1[0:-31] + "" \
-                             "\n"
-        str3 = html_string[i:]
-        j = str3.find("</span")
+        html_space_index = html_string.find("&#xa0;")
+        str1 = html_string[0:html_space_index]
+        str1 = str1[0:-31] + "\n"
+        str3 = html_string[html_space_index:]
+        end_point = str3.find("</span")
         str2 = ""
-        for k in range(0, int(j / 6)):
+        html_spaces_count = int(end_point / 6)
+        for k in range(0, html_spaces_count):
             str2 += " "
-        str3 = str3[j + 7:]
+        str3 = str3[end_point + 7:]
         html_string = str1 + str2 + str3
         for i in range(10, -1, -1):
             html_string = html_string.replace("</span>" + " " * i, " " * i + "</span>")
         html_string = html_string.replace("#40015a", "#e1a8ff")
-        st = html_string.split("</span>")
-        str = st[0] + "</span>"
-        for i in range(1, st.__len__()):
-            j = st[i].find("<span")
+        lines = html_string.split("</span>")
+        html_string = lines[0] + "</span>"
+        for i in range(1, lines.__len__()):
+            j = lines[i].find("<span")
             temp = ""
             starti = -1
             for k in range(0, j):
-                if (st[i][k] == "\n" or st[i][k] == " ") and starti == -1:
+                if (lines[i][k] == "\n" or lines[i][k] == " ") and starti == -1:
                     continue
                 else:
                     if starti == -1: starti = k
-                    temp = temp + st[i][k]
+                    temp = temp + lines[i][k]
             if temp != "":
-                temp = st[i][0:starti]
-                temp = temp + '<span style="color:#ffffff;">' + st[i][starti:j] + "</span>"
-                temp = temp + st[i][j:]
-                st[i] = temp
-            str = str + st[i] + "</span>"
-        html_string = str
+                temp = lines[i][0:starti]
+                temp = temp + '<span style="color:#ffffff;">' + lines[i][starti:j] + "</span>"
+                temp = temp + lines[i][j:]
+                lines[i] = temp
+            html_string = html_string + lines[i] + "</span>"
         lines = html_string.split("\n")
-        i = lines.index("</pre>")
-        lines = lines[0:i]
-        i = lines[0].find(">")
-        lines[0] = lines[0][i + 1:]
+        end_point = lines.index("</pre>")
+        lines = lines[0:end_point]
+        start_point = lines[0].find(">")
+        lines[0] = lines[0][start_point + 1:]
         code_json = []
         tab_spaces = []
-        k = -1
-        for i in range(0, lines.__len__()):
-            if lines[i].__len__() == 0:
+        code_json_line_index = -1
+        for line_index in range(0, lines.__len__()):
+            if lines[line_index].__len__() == 0:
                 continue
             code_json.append([])
-            k = k + 1
-            if lines[i].startswith(self.indentation_char):
-                j = lines[i].find("<")
-                lines[i] = "\t" * lines[i][:j].count(self.indentation_char) + lines[i][j:]
-            count = 0
-            while lines[i][count] == '\t':
-                count = count + 1
-            tab_spaces.append(count)
-            spans=lines[i].split("<span")
-            for j in range(1,spans.__len__()):
-                k1 = spans[j].find(":")
-                color = spans[j][k1+1:k1+8]
-                k1=spans[j].find(">")
-                k2=spans[j].find("</span>")
-                text = spans[j][k1+1:k2]
+            code_json_line_index = code_json_line_index + 1
+            if lines[line_index].startswith(self.indentation_char):
+                start_point = lines[line_index].find("<")
+                lines[line_index] = "\t" * lines[line_index][:start_point].count(self.indentation_char) + lines[line_index][start_point:]
+            indentation_char_count = 0
+            while lines[line_index][indentation_char_count] == '\t':
+                indentation_char_count = indentation_char_count + 1
+            tab_spaces.append(indentation_char_count)
+            words=lines[line_index].split("<span")
+            for word_index in range(1,words.__len__()):
+                color_index = words[word_index].find(":")
+                color = words[word_index][color_index+1:color_index+8]
+                start_point=words[word_index].find(">")
+                end_point=words[word_index].find("</span>")
+                text = words[word_index][start_point+1:end_point]
                 text = html.unescape(text)
-                code_json[k].append([text, color])
+                code_json[code_json_line_index].append([text, color])
         self.code_json = code_json
         self.tab_spaces = tab_spaces
