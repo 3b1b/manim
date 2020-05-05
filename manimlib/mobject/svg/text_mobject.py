d5fe88ab9a0f9f444420abc8fe856be61f3736b6
Tony031218 <975062472@qq.com>
2020-05-05 11:09:53 +0800

    fix: remove Space mobjects in Text to fix the bug of TransformText

--- manimlib/mobject/svg/text_mobject.py
+++ manimlib/mobject/svg/text_mobject.py
@@ -56,6 +56,7 @@ class Text(SVGMobject):
         nppc = self.n_points_per_cubic_curve
         for each in self:
             if len(each.points) == 0:
+                self.remove(each)
                 continue
             points = each.points
             last = points[0]
