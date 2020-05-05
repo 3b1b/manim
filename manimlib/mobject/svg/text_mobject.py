4b76d011f66c02fb8a673a640f091f6ac943f060
Tony031218 <975062472@qq.com>
2020-05-05 15:17:20 +0800

    this will lead to more bugs of setting color, so I delete it

--- manimlib/mobject/svg/text_mobject.py
+++ manimlib/mobject/svg/text_mobject.py
@@ -54,7 +54,6 @@ class Text(SVGMobject):
         nppc = self.n_points_per_curve
         for each in self:
             if len(each.points) == 0:
-                self.remove(each)
                 continue
             points = each.points
             last = points[0]
