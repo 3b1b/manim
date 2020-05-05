bb4658ec54faef31b6bd76cbdd9794c6c69e5a87
NavpreetDevpuri <30471072+NavpreetDevpuri@users.noreply.github.com>
2020-05-05 07:02:20 +0530

    Update code_mobject.py

--- manimlib/mobject/code_mobject.py
+++ manimlib/mobject/code_mobject.py
@@ -11,7 +11,7 @@ from pygments.lexers import get_lexer_by_name
 from pygments.formatters.html import HtmlFormatter
 
 '''
-coordinate point is LEFT+UP corner background_rect
+coordinate point is LEFT+UP corner of background_rect
 
 codemobject is VGroup() with three things
 codemobject[0] is codemobject.background_rect 
