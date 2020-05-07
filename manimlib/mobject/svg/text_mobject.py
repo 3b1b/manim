80eccfd2344df88b25714cce1835f8763a49751a
NavpreetDevpuri <30471072+NavpreetDevpuri@users.noreply.github.com>
2020-05-07 22:35:28 +0530

    Update text_mobject.py

--- manimlib/mobject/svg/text_mobject.py
+++ manimlib/mobject/svg/text_mobject.py
@@ -7,12 +7,22 @@ import manimlib.constants as consts
 from manimlib.constants import *
 from manimlib.container.container import Container
 from manimlib.mobject.geometry import Dot, Rectangle
-from manimlib.mobject.shape_matchers import SurroundingRectangle
 from manimlib.mobject.svg.svg_mobject import SVGMobject
 from manimlib.mobject.types.vectorized_mobject import VGroup
 from manimlib.utils.config_ops import digest_config
 
+
 TEXT_MOB_SCALE_FACTOR = 0.05
+
+
+class TextSetting(object):
+    def __init__(self, start, end, font, slant, weight, line_num=-1):
+        self.start = start
+        self.end = end
+        self.font = font
+        self.slant = slant
+        self.weight = weight
+        self.line_num = line_num
 
 
 class Text(SVGMobject):
@@ -38,30 +48,40 @@ class Text(SVGMobject):
         't2s': {},
         't2w': {},
         'tab_width': 4,
+        "exact_spaces": False
     }
 
     def __init__(self, text, **config):
         self.full2short(config)
         digest_config(self, config)
 
-        for i in range(text.__len__()):
-            if text[i] == "\t" or text[i] == " ":
-                continue
-            else:
-                break
-        first_visible_char_index = i
-        text_with_space = text[first_visible_char_index] + " " + text[first_visible_char_index]
-        self.space_width = self.get_space_width(text_with_space)
+        if self.exact_spaces == True:
+            for i in range(text.__len__()):
+                if text[i] == "\t" or text[i] == " ":
+                    continue
+                else:
+                    break
+            first_visible_char_index = i
+            text_with_space = text[first_visible_char_index] + " " + text[first_visible_char_index]
+            self.space_width = self.get_space_width(text_with_space)
+            text_without_tabs = text
+            if text.find('\t') != -1:
+                text_without_tabs = text.replace('\t', ' ' * self.tab_width)
+            self.text = text_without_tabs
+        else:
+            if text.find('\t') != -1:
+                text = text.replace('\t', ' ' * self.tab_width)
+            self.text = text
 
-        text_without_tabs = text
-        if text.find('\t') != -1:
-            text_without_tabs = text.replace('\t', ' ' * self.tab_width)
-        self.text = text_without_tabs
         self.lsh = self.size if self.lsh == -1 else self.lsh
+
         file_name = self.text2svg()
         self.remove_last_M(file_name)
         SVGMobject.__init__(self, file_name, **config)
-        # self.text = text_without_tabs
+        if self.exact_spaces == True:
+            self.apply_exact_space_chars()
+        else:
+            self.apply_space_chars()
 
         nppc = self.n_points_per_cubic_curve
         for each in self:
@@ -72,16 +92,11 @@ class Text(SVGMobject):
             each.clear_points()
             for index, point in enumerate(points):
                 each.append_points([point])
-                if index != len(points) - 1 and (index + 1) % nppc == 0 and any(point != points[index + 1]):
+                if index != len(points) - 1 and (index + 1) % nppc == 0 and any(point != points[index+1]):
                     each.add_line_to(last)
                     last = points[index + 1]
             each.add_line_to(last)
 
-        self.text = text
-        # anti-aliasing
-        self.scale(TEXT_MOB_SCALE_FACTOR)
-        if self.text.find("\t") != -1 or self.text.find(" ") != -1:
-            self.apply_space_chars()
         if self.t2c:
             self.set_color_by_t2c()
         if self.gradient:
@@ -89,7 +104,26 @@ class Text(SVGMobject):
         if self.t2g:
             self.set_color_by_t2g()
 
+        # anti-aliasing
+        if self.height is None and self.width is None:
+            self.scale(TEXT_MOB_SCALE_FACTOR)
+
     def apply_space_chars(self):
+        indexes = self.find_indexes(' ') + self.find_indexes('\n')
+        indexes = sorted(indexes, key=lambda i: i[0])
+        if len(self.text) == len(indexes):
+            space = Dot(fill_opacity=0, stroke_opacity=0)
+            self.submobjects = [space.copy() for _ in range(len(indexes))]
+            return
+        for start, _ in indexes:
+            space = Dot(fill_opacity=0, stroke_opacity=0)
+            if start == 0:
+                space.move_to(self.submobjects[0].get_center())
+            else:
+                space.move_to(self.submobjects[start-1].get_center())
+            self.submobjects.insert(start, space)
+
+    def apply_exact_space_chars(self):
         indexes = self.find_indexes(' ') + self.find_indexes("\t")
         indexes = sorted(indexes, key=lambda i: i[0])
         for i in range(self.text.__len__()):
@@ -139,7 +173,7 @@ class Text(SVGMobject):
     def remove_last_M(self, file_name):
         with open(file_name, 'r') as fpr:
             content = fpr.read()
-        content = re.sub(r'Z M [^[A-Za-z]*? "\/>', 'Z "/>', content)
+        content = re.sub(r'Z M [^A-Za-z]*? "\/>', 'Z "/>', content)
         with open(file_name, 'w') as fpw:
             fpw.write(content)
 
@@ -204,18 +238,69 @@ class Text(SVGMobject):
         settings = self.font + self.slant + self.weight
         settings += str(self.t2f) + str(self.t2s) + str(self.t2w)
         settings += str(self.lsh) + str(self.size)
-        id_str = self.text + settings
+        id_str = self.text+settings
         hasher = hashlib.sha256()
         hasher.update(id_str.encode())
         return hasher.hexdigest()[:16]
 
+    def text2settings(self):
+        settings = []
+        t2x = [self.t2f, self.t2s, self.t2w]
+        for i in range(len(t2x)):
+            fsw = [self.font, self.slant, self.weight]
+            if t2x[i]:
+                for word, x in list(t2x[i].items()):
+                    for start, end in self.find_indexes(word):
+                        fsw[i] = x
+                        settings.append(TextSetting(start, end, *fsw))
+
+        # Set All text settings(default font slant weight)
+        fsw = [self.font, self.slant, self.weight]
+        settings.sort(key=lambda setting: setting.start)
+        temp_settings = settings.copy()
+        start = 0
+        for setting in settings:
+            if setting.start != start:
+                temp_settings.append(TextSetting(start, setting.start, *fsw))
+            start = setting.end
+        if start != len(self.text):
+            temp_settings.append(TextSetting(start, len(self.text), *fsw))
+        settings = sorted(temp_settings, key=lambda setting: setting.start)
+
+        if re.search(r'\n', self.text):
+            line_num = 0
+            for start, end in self.find_indexes('\n'):
+                for setting in settings:
+                    if setting.line_num == -1:
+                        setting.line_num = line_num
+                    if start < setting.end:
+                        line_num += 1
+                        new_setting = copy.copy(setting)
+                        setting.end = end
+                        new_setting.start = end
+                        new_setting.line_num = line_num
+                        settings.append(new_setting)
+                        settings.sort(key=lambda setting: setting.start)
+                        break
+
+        for setting in settings:
+            if setting.line_num == -1:
+                setting.line_num = 0
+
+        return settings
+
     def text2svg(self):
         # anti-aliasing
         size = self.size * 10
+        lsh = self.lsh * 10
+
+        if self.font == '':
+            if NOT_SETTING_FONT_MSG != "":
+                print(NOT_SETTING_FONT_MSG)
 
         dir_name = consts.TEXT_DIR
         hash_name = self.text2hash()
-        file_name = os.path.join(dir_name, hash_name) + '.svg'
+        file_name = os.path.join(dir_name, hash_name)+'.svg'
         if os.path.exists(file_name):
             return file_name
 
@@ -223,11 +308,23 @@ class Text(SVGMobject):
         context = cairo.Context(surface)
         context.set_font_size(size)
         context.move_to(START_X, START_Y)
-        text = self.text
-        context.select_font_face(self.font, self.str2slant(self.slant), self.str2weight(self.weight))
-        context.move_to(START_X, START_Y)
-        context.show_text(text)
-        surface.finish()
+
+        settings = self.text2settings()
+        offset_x = 0
+        last_line_num = 0
+        for setting in settings:
+            font = setting.font
+            slant = self.str2slant(setting.slant)
+            weight = self.str2weight(setting.weight)
+            text = self.text[setting.start:setting.end].replace('\n', ' ')
+
+            context.select_font_face(font, slant, weight)
+            if setting.line_num != last_line_num:
+                offset_x = 0
+                last_line_num = setting.line_num
+            context.move_to(START_X + offset_x, START_Y + lsh*setting.line_num)
+            context.show_text(text)
+            offset_x += context.text_extents(text)[4]
 
         return file_name
 
@@ -245,15 +342,13 @@ class Text(SVGMobject):
         context.move_to(START_X, START_Y)
         context.show_text(text_with_space)
         surface.finish()
-        svg_with_space = SVGMobject(file_name, height=None,
-                                         width=None,
-                                         stroke_width=0,
-                                         should_center=True,
-                                         unpack_groups=True, )
-        svg_with_space.scale(TEXT_MOB_SCALE_FACTOR)
+        svg_with_space = SVGMobject(file_name, height=self.height,
+                                         width=self.width,
+                                         stroke_width=self.stroke_width,
+                                         should_center=self.should_center,
+                                         unpack_groups=self.unpack_groups, )
         space_width = svg_with_space[1].get_left()[0] - svg_with_space[0].get_right()[0]
         return space_width
-
 
 class TextWithFixHeight(Text):
     def __init__(self, text, **kwargs):
