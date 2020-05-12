0cab1862743b44504912213ba57260bad7ce2fbc
NavpreetDevpuri <30471072+NavpreetDevpuri@users.noreply.github.com>
2020-05-12 10:26:51 +0530

    Update imports.py

--- manimlib/imports.py
+++ manimlib/imports.py
@@ -50,7 +50,6 @@ from manimlib.mobject.svg.drawings import *
 from manimlib.mobject.svg.svg_mobject import *
 from manimlib.mobject.svg.tex_mobject import *
 from manimlib.mobject.svg.text_mobject import *
-from manimlib.mobject.svg.code_mobject import *
 from manimlib.mobject.three_d_utils import *
 from manimlib.mobject.three_dimensions import *
 from manimlib.mobject.types.image_mobject import *
