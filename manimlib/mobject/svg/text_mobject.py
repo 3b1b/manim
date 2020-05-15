b4381d268ddf564a3f90707d2b30877f45539465
NavpreetDevpuri <30471072+NavpreetDevpuri@users.noreply.github.com>
2020-05-15 11:45:56 +0530

    Update text_mobject.py

--- manimlib/mobject/svg/text_mobject.py
+++ manimlib/mobject/svg/text_mobject.py
@@ -1,14 +1,13 @@
 import re
-import os
 import copy
 import hashlib
 import cairo
 import manimlib.constants as consts
 from manimlib.constants import *
 from manimlib.container.container import Container
-from manimlib.mobject.geometry import Dot, Rectangle
+from manimlib.mobject.geometry import Dot
 from manimlib.mobject.svg.svg_mobject import SVGMobject
-from manimlib.mobject.types.vectorized_mobject import VGroup, VMobject
+from manimlib.mobject.types.vectorized_mobject import VGroup
 from manimlib.utils.config_ops import digest_config
 
 TEXT_MOB_SCALE_FACTOR = 0.05
