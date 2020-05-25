2ce0ffac0332597cb94ebc80b72f4b7bf8d2b2f1
safinsingh <safin.singh@gmail.com>
2020-05-24 21:22:16 -0700

    Add python -m on windows

--- .travis/windows.sh
+++ .travis/windows.sh
@@ -4,3 +4,5 @@ choco install python --version=$PYVER
 export PATH="/c/$PYDIR:/c/$PYDIR/Scripts:$PATH"
 cmd.exe //c "RefreshEnv.cmd"
 python -m pip install --upgrade pip
+python -m pip install --user -r requirements.txt
+python -m pip install --user .
