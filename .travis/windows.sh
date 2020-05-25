6c8a4754cc1493d8a8ff518521f5b5898b1a578f
safinsingh <safin.singh@gmail.com>
2020-05-24 21:38:19 -0700

    try to make cairo work with windoze??

--- .travis/windows.sh
+++
@@ -1,8 +0,0 @@
-##### THIS IS FOR TRAVIS BUILDS, DO NOT RUN THIS ON YOUR COMPUTER! #####
-
-choco install python --version=$PYVER
-export PATH="/c/$PYDIR:/c/$PYDIR/Scripts:$PATH"
-cmd.exe //c "RefreshEnv.cmd"
-python -m pip install --upgrade pip
-python -m pip install --user -r requirements.txt
-python -m pip install --user .
