4f9b9bb4f4982e3ed6c2df79fb9228a047e14547
Hugues Devimeux <thespourieye@gmail.com>
2020-06-02 22:36:54 +0200

    added set_test

---
+++ manim/utils/set_test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import os 
+import inspect
+
+def set_test_scene(scene_object, CONFIG, module_name): 
+    """Function used to set up the test data for a new feature. Please Refer to the wiki."""
+    scene = scene_object(**CONFIG)
+    data = scene.get_frame()
+    path = os.path.join("manim","tests","tests_data","{}".format(module_name))
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    np.save(os.path.join(path, str(scene)), data)
+    print('Saved in ' + path + '\n')
