4166f46081cf7e51a39ff98487acc6c756ad13d1
Devin Neal <devin@eulertour.com>
2020-06-03 00:27:40 -0700

    Revert "Merge pull request #48 from PhilippImhof/customtex"
    This reverts commit 9c8913ff0cd60811c7a4b06044e07920f871b94a, reversing
    changes made to a29eabdf877442d3bee89a1d66bd0ff721484221.

--- manim/__main__.py
+++ manim/__main__.py
@@ -7,7 +7,6 @@ def main():
     args = config.parse_cli()
     cfg = config.get_configuration(args)
     config.initialize_directories(cfg)
-    config.initialize_tex(cfg)
     extract_scene.main(cfg)
 
 
