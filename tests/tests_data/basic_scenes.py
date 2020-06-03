345175272c6e473ad3757e4dc45dd5fad48ffba2
Hugues Devimeux <thespourieye@gmail.com>
2020-06-03 22:27:46 +0200

    added test data

---
+++ tests/tests_data/basic_scenes.py
@@ -0,0 +1,31 @@
+from manim import *
+
+#This module is used for testing purpose only
+
+class SquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()
+        square = Square()
+        square.flip(RIGHT)
+        square.rotate(-3 * TAU / 8)
+        circle.set_fill(PINK, opacity=0.5)
+
+        self.play(ShowCreation(square))
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
+
+class WriteStuff(Scene): 
+    def construct(self): 
+        example_text = TextMobject(
+            "This is a some text",
+            tex_to_color_map={"text": YELLOW}
+        )
+        example_tex = TexMobject(
+            "\\sum_{k=1}^\\infty {1 \\over k^2} = {\\pi^2 \\over 6}",
+        )
+        group = VGroup(example_text, example_tex)
+        group.arrange(DOWN)
+        group.set_width(FRAME_WIDTH - 2 * LARGE_BUFF)
+
+        self.play(Write(example_text))
+        self.play(Write(example_tex))
