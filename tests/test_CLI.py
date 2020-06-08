7afa9897cd0804665ad14633e0731e51ff23bfc5
Hugues Devimeux <thespourieye@gmail.com>
2020-06-08 11:07:44 +0200

    changed python3 to python (test)

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -13,7 +13,7 @@ def capture(command):
 
 
 def test_help():
-    command = ["python3", "-m", "manim", "--help"]
+    command = ["python", "-m", "manim", "--help"]
     out, err, exitcode = capture(command)
     assert exitcode == 0, "Manim is not correcly installed. Please refer to the troubleshooting section on the wiki. Error : {}".format(
         err)
@@ -23,7 +23,7 @@ def test_basicScene():
     """ Simulate SquareToCircle. The cache will be saved in tests_caches/media_temp (temporary directory). This is mainly intended to test the partial-movies process. """
     path_basic_scene = os.path.join("tests", "tests_data", "basic_scenes.py")
     path_output = os.path.join("tests", "tests_cache", "media_temp")
-    command = ["python3", "-m", "manim", path_basic_scene,
+    command = ["python", "-m", "manim", path_basic_scene,
                "SquareToCircle", "-l", "--media_dir", path_output]
     out, err, exitcode = capture(command)
     assert exitcode == 0, err
@@ -36,7 +36,7 @@ def test_WriteStuff():
     """Simulate WriteStuff. This is mainly intended to test the caching process of the tex objects"""
     path_basic_scene = os.path.join("tests", "tests_data", "basic_scenes.py")
     path_output = os.path.join("tests", "tests_cache", "media_temp")
-    command = ["python3", "-m", "manim", path_basic_scene,
+    command = ["python", "-m", "manim", path_basic_scene,
                "WriteStuff", "-l", "--media_dir", path_output]
     out, err, exitcode = capture(command)
     assert exitcode == 0, err
