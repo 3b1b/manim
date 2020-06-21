ef54471e6012a141442ce9a5293e0718fe0bc2f6
Navpreet Devpuri <navpreetdevpuri@gmail.com>
2020-06-21 07:52:44 +0530

    Update code_mobject.py

--- manimlib/mobject/svg/code_mobject.py
+++ manimlib/mobject/svg/code_mobject.py
@@ -16,10 +16,9 @@ from pygments.styles import get_all_styles
 '''
 Code.styles_list static variable is containing list of names of all styles 
 Code is VGroup() with three things
-    Code[0] is Code.background_mobject
-        which can be a 
-            Rectangle() if background == "rectangle" 
-            VGroup() of Rectangle() and Dot() for three buttons if background == "window" 
+    Code[0] is Code.background_mobject is a VGroup()
+        VGroup() of SurroundingRectangle() if background == "rectangle" 
+        VGroup() of RoundedRectangle() and Dot() for three buttons if background == "window" 
     Code[1] is Code.line_numbers Which is a Paragraph() object, this mean you can use 
                 Code.line_numbers[0] or Code.line_numbers.chars[0] or Code[1].chars[0] to access first line number 
     Code[2] is Code.code
@@ -39,6 +38,60 @@ or remove_invisible_chars(Code.code) or remove_invisible_chars(Code)
 
 
 class Code(VGroup):
+    """Class Code is used to display code with color highlighted.
+
+    Parameters
+    ----------
+    file_name : :class:`str`
+        Name of the code file to display.
+    tab_width : :class:`int`, optional (default is 3).
+        Number of space characters for a tab character.
+    line_spacing : :class:`float`, optional (default is 0.3, which means 30% of font size)
+        Amount of space between lines in respect of font size.
+    scale_factor : class:`float`, optional (default is 0.5)
+        It is a number which scales displayed code.
+    font : :class:`str`, optional (default is 'Monospac821 BT')
+         Name of text font.
+    stroke_width : class:`float`, optional (default is 0)
+        Stroke width for text. 0 is recommended.
+    margin: class :`float`, optional (default is 0.3)
+        Inner margin of text from background.
+    indentation_chars : :class:`str`, optional (default is "    ")
+        Indentation chars refers to the spaces/tabs at the beginning of given code line.
+    background : :class:`str`, optional (default is "rectangle")
+        It defines background type. Currently supports only "rectangle" and "window".
+    background_stroke_width : class:`float`, optional (default is 1)
+        It defines stroke width of background.
+    background_stroke_color : class:`str`, optional (default is WHITE)
+        It defines stroke color for background.
+    corner_radius : :class:`float`, optional (default is 0.2)
+        It defines corner radius for background.
+    insert_line_no : :class:`bool`, optional (default is True)
+        It defines whether insert line numbers in displayed code.
+    line_no_from : :class:`int`, optional (default is 1)
+        It defines starting number for counting lines.
+    line_no_buff : :class:`float`, optional (default is 0.4)
+        It defines space between line numbers and displayed code.
+    style : :class:`str`, optional (default is 'vim')
+        It defines style of code displayed. You can see names of styles from Code.styles_list.
+    language : :class:`str`, optional (default is None, Which mean it will automatically detects the language)
+        It defines the language name of given code.
+        For Knowing more available options visit https://pygments.org/docs/lexers/  for 'aliases or short names'
+    generate_html_file : :class:`bool`, optional (default is False)
+        It defines whether to generate code highlighted html to folder assets/codes/generated_html_files.
+
+    Attributes (It have all Parameters as Attributes and some extra as following)
+    ----------
+    background_mobject : :class:`~.VGroup`
+        To display background according to background type specified by background in Parameters.
+        VGroup with SurroundingRectangle() if background == "rectangle"
+        VGroup with RoundedRectangle() and Dot() for three buttons if background == "window"
+    line_numbers : :class:`~.Paragraph`
+        To display line numbers of displayed code if insert_line_no == True.
+    code : :class:`~.Paragraph`
+        To display highlighted code.
+    """
+
     # tuples in the form (name, aliases, filetypes, mimetypes)
     # 'language' of CONFIG is aliases or short names
     # For more information about pygments.lexers visit https://pygments.org/docs/lexers/
@@ -48,13 +101,12 @@ class Code(VGroup):
     # For more information about pygments.styles visit https://pygments.org/docs/styles/
     CONFIG = {
         "tab_width": 3,
-        "line_spacing": -1,
+        "line_spacing": 0.3,
         "scale_factor": 0.5,
-        "run_time": 1,
         "font": 'Monospac821 BT',
         'stroke_width': 0,
         'margin': 0.3,
-        'indentation_char': "    ",
+        'indentation_chars': "    ",
         "background": "rectangle",  # or window
         "background_stroke_width": 1,
         "background_stroke_color": WHITE,
@@ -67,9 +119,8 @@ class Code(VGroup):
         'generate_html_file': False
     }
 
-    def __init__(self, file_name=None, **kwargs):
+    def __init__(self, **kwargs):
         Container.__init__(self, **kwargs)
-        self.file_name = file_name or self.file_name
         self.ensure_valid_file()
         self.style = self.style.lower()
         self.gen_html_string()
@@ -87,13 +138,14 @@ class Code(VGroup):
                 forground = VGroup(self.code, self.line_numbers)
             else:
                 forground = self.code
-            self.background_mobject = SurroundingRectangle(forground, buff=self.margin,
-                                                           color=self.background_color,
-                                                           fill_color=self.background_color,
-                                                           stroke_width=self.background_stroke_width,
-                                                           stroke_color=self.background_stroke_color,
-                                                           fill_opacity=1, )
-            self.background_mobject.round_corners(self.corner_radius)
+            rect = SurroundingRectangle(forground, buff=self.margin,
+                                        color=self.background_color,
+                                        fill_color=self.background_color,
+                                        stroke_width=self.background_stroke_width,
+                                        stroke_color=self.background_stroke_color,
+                                        fill_opacity=1, )
+            rect.round_corners(self.corner_radius)
+            self.background_mobject = VGroup(rect)
         else:
             if self.insert_line_no:
                 forground = VGroup(self.code, self.line_numbers)
@@ -103,10 +155,10 @@ class Code(VGroup):
             height = forground.get_height() + 0.1 * 3 + 2 * self.margin
             width = forground.get_width() + 0.1 * 3 + 2 * self.margin
 
-            rrect = RoundedRectangle(corner_radius=self.corner_radius, height=height, width=width,
-                                     stroke_width=self.background_stroke_width,
-                                     stroke_color=self.background_stroke_color,
-                                     color=self.background_color, fill_opacity=1)
+            rect = RoundedRectangle(corner_radius=self.corner_radius, height=height, width=width,
+                                    stroke_width=self.background_stroke_width,
+                                    stroke_color=self.background_stroke_color,
+                                    color=self.background_color, fill_opacity=1)
             red_button = Dot(radius=0.1, stroke_width=0, color='#ff5f56')
             red_button.shift(LEFT * 0.1 * 3)
             yellow_button = Dot(radius=0.1, stroke_width=0, color='#ffbd2e')
@@ -116,7 +168,7 @@ class Code(VGroup):
             buttons.shift(
                 UP * (height / 2 - 0.1 * 2 - 0.05) + LEFT * (width / 2 - 0.1 * 5 - self.corner_radius / 2 - 0.05))
 
-            self.background_mobject = VGroup(rrect, buttons)
+            self.background_mobject = VGroup(rect, buttons)
             x = (height - forground.get_height()) / 2 - 0.1 * 3
             self.background_mobject.shift(forground.get_center())
             self.background_mobject.shift(UP * x)
@@ -128,18 +180,10 @@ class Code(VGroup):
 
         self.move_to(np.array([0, 0, 0]))
 
-    def apply_points_function_about_point(self, func, about_point=None, about_edge=None):
-        if about_point is None:
-            if about_edge is None:
-                about_edge = self.get_corner(UP + LEFT)
-            about_point = self.get_critical_point(about_edge)
-        for mob in self.family_members_with_points():
-            mob.points -= about_point
-            mob.points = func(mob.points)
-            mob.points += about_point
-        return self
-
     def ensure_valid_file(self):
+        """Function to validate file.
+        """
+
         if self.file_name is None:
             raise Exception("Must specify file for Code")
         possible_paths = [
@@ -154,6 +198,13 @@ class Code(VGroup):
                       self.file_name)
 
     def gen_line_numbers(self):
+        """Function to generate line_numbers.
+
+        Returns
+        -------
+        :class:`~.Paragraph`
+            The generated line_numbers according to parameters.
+        """
         line_numbers_array = []
         for line_no in range(0, self.code_json.__len__()):
             number = str(self.line_no_from + line_no)
@@ -166,6 +217,13 @@ class Code(VGroup):
         return line_numbers
 
     def gen_colored_lines(self):
+        """Function to generate code.
+
+        Returns
+        -------
+        :class:`~.Paragraph`
+            The generated code according to parameters.
+        """
         lines_text = []
         for line_no in range(0, self.code_json.__len__()):
             line_str = ""
@@ -184,11 +242,14 @@ class Code(VGroup):
         return code
 
     def gen_html_string(self):
+        """Function to generate html string with code highlighted and stores in variable html_string.
+        """
         file = open(self.file_path, "r")
         code_str = file.read()
         file.close()
-        self.html_string = hilite_me(code_str, self.language, {}, self.style, self.insert_line_no,
-                                         "border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em .6em;" , self.file_path)
+        self.html_string = hilite_me(code_str, self.language, self.style, self.insert_line_no,
+                                     "border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em .6em;",
+                                     self.file_path)
 
         if self.generate_html_file:
             os.makedirs(os.path.join("assets", "codes", "generated_html_files"), exist_ok=True)
@@ -197,6 +258,13 @@ class Code(VGroup):
             file.close()
 
     def gen_code_json(self):
+        """Function to background_color, generate code_json and tab_spaces from html_string.
+        background_color is just background color of displayed code.
+        code_json is 2d array with rows as line numbers
+        and columns as a array with length 2 having text and text's color value.
+        tab_spaces is 2d array with rows as line numbers
+        and columns as corresponding number of indentation_chars in front of that line in code.
+        """
         if self.background_color == "#111111" or \
                 self.background_color == "#272822" or \
                 self.background_color == "#202020" or \
@@ -232,21 +300,21 @@ class Code(VGroup):
             # print(lines[line_index])
             self.code_json.append([])
             code_json_line_index = code_json_line_index + 1
-            if lines[line_index].startswith(self.indentation_char):
+            if lines[line_index].startswith(self.indentation_chars):
                 start_point = lines[line_index].find("<")
                 starting_string = lines[line_index][:start_point]
-                indentation_char_count = lines[line_index][:start_point].count(self.indentation_char)
-                if starting_string.__len__() != indentation_char_count * self.indentation_char.__len__():
-                    lines[line_index] = "\t" * indentation_char_count + starting_string[starting_string.rfind(
-                        self.indentation_char) + self.indentation_char.__len__():] + \
+                indentation_chars_count = lines[line_index][:start_point].count(self.indentation_chars)
+                if starting_string.__len__() != indentation_chars_count * self.indentation_chars.__len__():
+                    lines[line_index] = "\t" * indentation_chars_count + starting_string[starting_string.rfind(
+                        self.indentation_chars) + self.indentation_chars.__len__():] + \
                                         lines[line_index][start_point:]
                 else:
-                    lines[line_index] = "\t" * indentation_char_count + lines[line_index][start_point:]
+                    lines[line_index] = "\t" * indentation_chars_count + lines[line_index][start_point:]
 
-            indentation_char_count = 0
-            while lines[line_index][indentation_char_count] == '\t':
-                indentation_char_count = indentation_char_count + 1
-            self.tab_spaces.append(indentation_char_count)
+            indentation_chars_count = 0
+            while lines[line_index][indentation_chars_count] == '\t':
+                indentation_chars_count = indentation_chars_count + 1
+            self.tab_spaces.append(indentation_chars_count)
             # print(lines[line_index])
             lines[line_index] = self.correct_non_span(lines[line_index])
             # print(lines[line_index])
@@ -269,6 +337,12 @@ class Code(VGroup):
         # print(self.code_json)
 
     def correct_non_span(self, line_str):
+        """Function put text color to those strings that don't have one according to background_color of displayed code.
+        Arguments
+        ---------
+        line_str : :class:`str`
+            Takes a string to put color to it according to background_color of displayed code.
+        """
         words = line_str.split("</span>")
         line_str = ""
         for i in range(0, words.__len__()):
@@ -296,7 +370,24 @@ class Code(VGroup):
         return line_str
 
 
-def hilite_me(code, lexer, options, style, linenos, divstyles, file_path):
+def hilite_me(code, language, style, insert_line_no, divstyles, file_path):
+    """Function to highlight code from string to html.
+
+    Arguments
+    ---------
+    code : :class:`str`
+        Code string.
+    language : :class:`str`
+        Code language name.
+    style : :class:`str`
+        Code style name.
+    insert_line_no : :class:`bool`
+        It defines whether insert line numbers in html file.
+    divstyles : :class:`str`
+        Some html css styles.
+    file_path : :class:`str`
+        Path of code file.
+       """
     style = style or 'colorful'
     defstyles = 'overflow:auto;width:auto;'
 
@@ -306,22 +397,25 @@ def hilite_me(code, lexer, options, style, linenos, divstyles, file_path):
                               cssclass='',
                               cssstyles=defstyles + divstyles,
                               prestyles='margin: 0')
-    if lexer == None:
+    if language is None:
         lexer = guess_lexer_for_filename(file_path, code)
         html = highlight(code, lexer, formatter)
     else:
-        html = highlight(code, get_lexer_by_name(lexer, **options), formatter)
-    if linenos:
+        html = highlight(code, get_lexer_by_name(language, **{}), formatter)
+    if insert_line_no:
         html = insert_line_numbers(html)
     html = "<!-- HTML generated by Code() -->" + html
     return html
 
 
-def get_default_style():
-    return 'border:solid gray;border-width:.1em .1em .1em .8em;padding:.2em .6em;'
-
-
 def insert_line_numbers(html):
+    """Function put line numbers to html of highlighted code.
+
+    Arguments
+    ---------
+    html : :class:`str`
+        html string of highlighted code.
+    """
     match = re.search('(<pre[^>]*>)(.*)(</pre>)', html, re.DOTALL)
     if not match: return html
 
