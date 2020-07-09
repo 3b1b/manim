75ebd56029a1da34f7813e2a6c63925d9c90df23
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-07-09 19:16:56 +0530

    Raise an exception if file is not a python script.

--- manim/__main__.py
+++ manim/__main__.py
@@ -135,6 +135,8 @@ def get_module(file_name):
             sys.exit(2)
     else:
         if os.path.exists(file_name):
+            if file_name[-3:] != ".py":
+                raise Exception(f"{file_name} is not a valid Manim python script.")
             module_name = file_name[:-3].replace(os.sep, '.').split('.')[-1]
             spec = importlib.util.spec_from_file_location(module_name, file_name)
             module = importlib.util.module_from_spec(spec)
