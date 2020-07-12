a1dae6d7fb522822f9121ec4d1e1651ae0bb2856
Devin Neal <devin@eulertour.com>
2020-07-12 01:02:43 -0700

    Remove travis files

--- .travis/osx.sh
+++
@@ -1,8 +0,0 @@
-##### THIS IS FOR TRAVIS BUILDS, DO NOT RUN THIS ON YOUR COMPUTER! #####
-
-pyenv install $PYVER
-export PYENV_VERSION=$PYVER
-export PATH="/Users/travis/.pyenv/shims:${PATH}"
-pyenv virtualenv venv
-source ~/.pyenv/versions/venv/bin/activate
-python --version
