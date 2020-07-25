4f390362318100a81837c175d17cc93197bcc6ad
azarzadavila <37216245+azarzadavila@users.noreply.github.com>
2020-07-25 15:19:13 +0200

    Show ouput of open_file_if_needed only on DEBUG

--- manim/__main__.py
+++ manim/__main__.py
@@ -17,7 +17,7 @@ from .logger import logger,console
 
 
 def open_file_if_needed(file_writer):
-    if file_writer_config["quiet"]:
+    if file_writer_config["verbose"] != "DEBUG":
         curr_stdout = sys.stdout
         sys.stdout = open(os.devnull, "w")
 
@@ -56,7 +56,7 @@ def open_file_if_needed(file_writer):
                 sp.call(commands, stdout=FNULL, stderr=sp.STDOUT)
                 FNULL.close()
 
-    if file_writer_config["quiet"]:
+    if file_writer_config["verbose"] != "DEBUG":
         sys.stdout.close()
         sys.stdout = curr_stdout
 
