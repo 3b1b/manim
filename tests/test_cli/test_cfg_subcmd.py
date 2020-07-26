e885280cc93d9584f2f8e9aee02b63016e256768
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-07-27 02:47:52 +0530

    Properly change cwd and change back as required.

--- tests/test_cli/test_cfg_subcmd.py
+++ tests/test_cli/test_cfg_subcmd.py
@@ -5,19 +5,18 @@ import shutil
 from test_cli import capture
 
 def test_cfg_help(python_version):
+    os.chdir(os.path.dirname(__file__))
     command = [python_version, "-m", "manim", "cfg", "--help"]
     out, err, exitcode = capture(command)
     assert exitcode == 0, f"The cfg subcommand is not working as intended."
 
 def test_cfg_show(python_version):
-    os.chdir(os.path.dirname(__file__))
     command = [python_version, "-m", "manim", "cfg", "show"]
     out, err, exitcode = capture(command)
     assert exitcode == 0
     assert f"{os.path.sep}tests{os.path.sep}".encode("utf-8") in out, err
 
 def test_cfg_export(python_version):
-    os.chdir(os.path.dirname(__file__))
     command = [python_version, "-m", "manim", "cfg", "export", "--dir", "temp"]
     out, err, exitcode = capture(command)
     assert exitcode == 0
