1026e1211b5768f4fbf322d3ffede78f070b8756
Ravi Arora <46346108+raviarora1209@users.noreply.github.com>
2020-08-09 15:19:48 +0530

    Updated line_intersection.py

--- manimlib/utils/space_ops.py
+++ manimlib/utils/space_ops.py
@@ -215,8 +215,8 @@ def line_intersection(line1, line2):
     each defined with a pair of vectors determining
     the end points
     """
-    x_diff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
-    y_diff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+    x_diff = (line1.get_start()[0] - line1.get_end()[0], line2.get_start()[0] - line2.get_end()[0])
+    y_diff = (line1.get_start()[1] - line1.get_end()[1], line2.get_start()[1] - line2.get_end()[1])
 
     def det(a, b):
         return a[0] * b[1] - a[1] * b[0]
@@ -224,7 +224,7 @@ def line_intersection(line1, line2):
     div = det(x_diff, y_diff)
     if div == 0:
         raise Exception("Lines do not intersect")
-    d = (det(*line1), det(*line2))
+    d = (det(*line1.get_start_and_end()), det(*line2.get_start_and_end()))
     x = det(d, x_diff) / div
     y = det(d, y_diff) / div
     return np.array([x, y, 0])
