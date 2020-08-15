7db6e6a7c0926ca0c00724a3fa7749bc26fa4294
kolibril13 <jan-hendrik.mueller@gmx.net>
2020-08-15 15:55:07 +0200

    generalized the script for all users

--- docs/source/make_and_open_docs.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import os
-import webbrowser
-
-path_makefile= Path().home() / "projects/manim-community/docs"
-os.system(f"cd {path_makefile} && make html")
-
-website = Path().home() / "projects/manim-community/docs/build/html/index.html"
-webbrowser.get('firefox').open_new_tab(f"{website}")
