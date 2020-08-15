7374c7b564ba8edc1b0c39a5670f2cd88be71ef6
leotrs <leo@leotrs.com>
2020-08-14 21:59:38 -0400

    black

--- manim/container/container.py
+++ manim/container/container.py
@@ -6,7 +6,7 @@ Abstract base class for several objects used by manim.  In particular, both
 from abc import ABC, abstractmethod
 from ..utils.config_ops import digest_config
 
-__all__ = ['Container']
+__all__ = ["Container"]
 
 
 class Container(ABC):
