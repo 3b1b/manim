353dfdc40307e585a49df5549675f95bc8e8dc71
Aathish Sivasubrahmanian <aathish04@gmail.com>
2020-08-25 20:33:09 +0530

    Add a way to pass a custom browser if wanted. (#343)
    You can now use `python make_and_open_docs.py <browser>` to open the docs in <browser>

--- scripts/make_and_open_docs.py
+++ scripts/make_and_open_docs.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import os
+import sys
 import webbrowser
 
 path_makefile = Path(__file__).parents[1] / "docs"
 os.system(f"cd {path_makefile} && make html")
 
-website = path_makefile / "build" / "html" / "index.html"
-webbrowser.open_new_tab(f"{website}")
-#alternative to the upper line:
-# webbrowser.get('firefox').open_new_tab(f"{website}")
+website = "file://" + str(path_makefile / "build" / "html" / "index.html")
+try:  # Allows you to pass a custom browser if you want.
+    webbrowser.get(sys.argv[1]).open_new_tab(f"{website}")
+except IndexError:
+    webbrowser.open_new_tab(f"{website}")
