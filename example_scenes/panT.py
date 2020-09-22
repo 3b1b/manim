47ef49428f36f2ce2e26dba7608be766c4c76232
Naveen <naveen@syrusdark.website>
2020-09-23 01:45:37 +0530

    Add more examples

--- example_scenes/panT.py
+++ example_scenes/panT.py
@@ -5,13 +5,21 @@ class Demo(Scene):
         'font' : 'sans-serif'
     }
     def construct(self):
-        text = PangoText('見 角 言 谷 豆 豕 豸 貝 赤 走 足 身 車 辛 辰 辵 邑 酉 釆 里!')
+        text = PangoText('見 角 言 谷  辛 辰 辵 邑 酉 釆 里!')
         self.play(Write(text))
         self.wait(2)
         mess=PangoText('multi-language',style=ITALIC)
         self.play(Transform(text,mess))
         self.clear()
-        #arb=PangoText('नमस्ते',font='sans-serif')
-        arb=PangoText("Здравствуйте")
+        #arb=PangoText('hi नमस्ते.',font='sans-serif')
+        print(len("नमस्ते"))
+        arb=PangoText("Здравствуйте मस नम म ",font='sans-serif')
         self.play(Write(arb))
         self.wait(4)
+class Demo1(Scene):
+    CONFIG = {
+        'font' : 'sans-serif'
+    }
+    def construct(self):
+        text = PangoText('Hello, world!', t2c={'world':BLUE})
+        self.play(Write(text))
