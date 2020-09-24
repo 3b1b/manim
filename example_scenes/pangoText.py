b1d1c19714800306b0d5f0295ea6d2d451f778cd
Naveen <naveen@syrusdark.website>
2020-09-24 23:00:09 +0530

    Run black

--- example_scenes/pangoText.py
+++ example_scenes/pangoText.py
@@ -5,7 +5,7 @@ class PangoRendering(Scene):
     CONFIG = {"font": "sans-serif"}
 
     def construct(self):
-        morning = PangoText("வணக்கம்",font="sans-serif")
+        morning = PangoText("வணக்கம்", font="sans-serif")
         self.play(Write(morning))
         self.wait(3)
         self.clear()
@@ -16,19 +16,21 @@ class PangoRendering(Scene):
         self.play(Transform(chin, mess))
         self.wait(2)
         self.clear()
-        russ = PangoText("Здравствуйте मस नम म ",font="sans-serif")
+        russ = PangoText("Здравствуйте मस नम म ", font="sans-serif")
         self.play(Write(russ))
         self.wait(2)
         self.clear()
-        hin = PangoText("नमस्ते",font="sans-serif")
+        hin = PangoText("नमस्ते", font="sans-serif")
         self.play(Write(hin))
         self.wait(2)
         self.clear()
-        arb = PangoText("صباح الخير  ",font="sans-serif")
+        arb = PangoText("صباح الخير  ", font="sans-serif")
         self.play(Write(arb))
         self.wait(3)
         self.clear()
-        arb = PangoText("صباح الخير \n تشرفت بمقابلتك",font="sans-serif") # don't mix RTL and LTR languages nothing shows up then ;-)
+        arb = PangoText(
+            "صباح الخير \n تشرفت بمقابلتك", font="sans-serif"
+        )  # don't mix RTL and LTR languages nothing shows up then ;-)
         self.play(Write(arb))
         self.wait(3)
         self.clear()
