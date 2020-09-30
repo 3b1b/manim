1d6290981500d0914cf3633ccfd9849fb4c0b96f
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2020-09-30 15:26:18 +0200

    Update color.py

--- manim/utils/color.py
+++ manim/utils/color.py
@@ -104,8 +104,9 @@ class Colors(Enum):
 
     .. code-block:: python
 
-       import manim.utils.color as C
-       C.WHITE   # -> '#FFFFFF'
+        >>> import manim.utils.color as C
+        >>> C.WHITE   
+        '#FFFFFF'
 
     Note this way uses the name of the colors in UPPERCASE.
 
