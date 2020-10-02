f41123c9302f5fea9087df1ce3864df2e0149908
Yuan <504567327@qq.com>
2020-10-02 14:50:16 +0800

    Revert-Chinese-support

--- manimlib/constants.py
+++ manimlib/constants.py
@@ -74,7 +74,7 @@ ITALIC = 'ITALIC'
 OBLIQUE = 'OBLIQUE'
 BOLD = 'BOLD'
 
-TEX_USE_CTEX = True
+TEX_USE_CTEX = False
 TEX_TEXT_TO_REPLACE = "YourTextHere"
 TEMPLATE_TEX_FILE = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
