dea6ced10c47e9d19c8eeb635e8e5aa7e619aafe
nilaybhatia <bhatianilay@gmail.com>
2020-10-05 13:54:48 +0530

    Add rest of the easing funcs

--- manim/utils/rate_functions.py
+++ manim/utils/rate_functions.py
@@ -1,4 +1,8 @@
-"""A selection of rate functions, i.e., *speed curves* for animations."""
+from math import sqrt
+
+
+"""A selection of rate functions, i.e., *speed curves* for animations.
+Please find a standard list at https://easings.net/"""
 
 __all__ = [
     "linear",
@@ -116,11 +120,179 @@ def exponential_decay(t, half_life=0.1):
     # the cut-off error at the end
     return 1 - np.exp(-t / half_life)
 
-def ease_in_sine(t):
+
+def ease_in_sin(t):
     return 1 - np.cos((t * np.pi) / 2)
 
-def ease_out_sine(t):
+
+def ease_out_sin(t):
     return np.sin((t * np.pi) / 2)
 
-def ease_in_out_sine(t):
+
+def ease_in_out_sin(t):
     return -(np.cos(np.pi * t) - 1) / 2
+
+
+def ease_in_quad(t):
+    return t * t
+
+
+def ease_out_quad(t):
+    return 1 - (1 - t) * (1 - t)
+
+
+def ease_in_out_quad(t):
+    return 2 * t * t if t < 0.5 else 1 - pow(-2 * t + 2, 2) / 2
+
+
+def ease_in_cubic(t):
+    return t * t * t
+
+
+def ease_out_cubic(t):
+    return 1 - pow(1 - t, 3)
+
+
+def ease_in_out_cubic(t):
+    return 4 * t * t * t if t < 0.5 else 1 - pow(-2 * t + 2, 3) / 2
+
+
+def ease_in_quart(t):
+    return t * t * t * t
+
+
+def ease_out_quart(t):
+    return 1 - pow(1 - t, 4)
+
+
+def ease_in_out_quart(t):
+    return 8 * t * t * t * t if t < 0.5 else 1 - pow(-2 * t + 2, 4) / 2
+
+
+def ease_in_quint(t):
+    return t * t * t * t * t
+
+
+def ease_out_quint(t):
+    return 1 - pow(1 - t, 5)
+
+
+def ease_in_out_quint(t):
+    return 16 * t * t * t * t * t if t < 0.5 else 1 - pow(-2 * t + 2, 5) / 2
+
+
+def ease_in_expo(t):
+    return 0 if t == 0 else pow(2, 10 * t - 10)
+
+
+def ease_out_expo(t):
+    return 1 if t == 1 else 1 - pow(2, -10 * t)
+
+
+def ease_in_out_expo(t):
+    if t == 0:
+        return 0
+    elif t == 1:
+        return 1
+    elif t < 0.5:
+        return pow(2, 20 * t - 10) / 2
+    else:
+        return 2 - pow(2, -20 * t + 10) / 2
+
+
+def ease_in_circ(t):
+    return 1 - sqrt(1 - pow(t, 2))
+
+
+def ease_out_circ(t):
+    return sqrt(1 - pow(t - 1, 2))
+
+
+def ease_in_out_circ(t):
+    return (
+        (1 - sqrt(1 - pow(2 * t, 2))) / 2
+        if t < 0.5
+        else (sqrt(1 - pow(-2 * t + 2, 2)) + 1) / 2
+    )
+
+
+def ease_in_back(t):
+    c1 = 1.70158
+    c3 = c1 + 1
+    return c3 * t * t * t - c1 * t * t
+
+
+def ease_out_back(t):
+    c1 = 1.70158
+    c3 = c1 + 1
+    return 1 + c3 * pow(t - 1, 3) + c1 * pow(t - 1, 2)
+
+
+def ease_in_out_back(t):
+    c1 = 1.70158
+    c2 = c1 * 1.525
+    return (
+        (pow(2 * t, 2) * ((c2 + 1) * 2 * t - c2)) / 2
+        if t < 0.5
+        else (pow(2 * t - 2, 2) * ((c2 + 1) * (t * 2 - 2) + c2) + 2) / 2
+    )
+
+
+def ease_in_elastic(t):
+    c4 = (2 * np.pi) / 3
+    if t == 0:
+        return 0
+    elif t == 1:
+        return 1
+    else:
+        return -pow(2, 10 * t - 10) * np.sin((t * 10 - 10.75) * c4)
+
+
+def ease_out_elastic(t):
+    c4 = (2 * np.pi) / 3
+    if t == 0:
+        return 0
+    elif t == 1:
+        return 1
+    else:
+        return pow(2, -10 * t) * np.sin((t * 10 - 0.75) * c4) + 1
+
+
+def ease_in_out_elastic(t):
+    c5 = (2 * np.pi) / 4.5
+    if t == 0:
+        return 0
+    elif t == 1:
+        return 1
+    elif t < 0.5:
+        return -(pow(2, 20 * t - 10) * np.sin((20 * t - 11.125) * c5)) / 2
+    else:
+        return (pow(2, -20 * t + 10) * np.sin((20 * t - 11.125) * c5)) / 2 + 1
+
+
+def ease_in_bounce(t):
+    return 1 - ease_out_bounce(1 - t)
+
+
+def ease_out_bounce(t):
+    n1 = 7.5625
+    d1 = 2.75
+
+    if t < 1 / d1:
+        return n1 * t * t
+    elif t < 2 / d1:
+        return n1 * (t - 1.5 / d1) * t + 0.75
+    elif t < 2.5 / d1:
+        return n1 * (t - 2.25 / d1) * t + 0.9375
+    else:
+        return n1 * (t - 2.625 / d1) * t + 0.984375
+
+
+def ease_in_out_bounce(t):
+    c1 = 1.70158
+    c2 = c1 * 1.525
+    return (
+        (pow(2 * t, 2) * ((c2 + 1) * 2 * t - c2)) / 2
+        if t < 0.5
+        else (pow(2 * t - 2, 2) * ((c2 + 1) * (t * 2 - 2) + c2) + 2) / 2
+    )
