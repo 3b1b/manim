e8a7593de609b127d51b9b7053b27bcc6eaa3e5f
nilaybhatia <bhatianilay@gmail.com>
2020-10-06 21:22:07 +0530

    Move example to rate_functions.py

--- manim/utils/rate_functions.py
+++ manim/utils/rate_functions.py
@@ -1,5 +1,33 @@
 """A selection of rate functions, i.e., *speed curves* for animations.
-Please find a standard list at https://easings.net/"""
+Please find a standard list at https://easings.net/
+
+class RateFunctions1Example(Scene):
+        def construct(self):
+            line1 = Line(3*LEFT, 3*RIGHT).shift(UP).set_color(RED)
+            line2 = Line(3*LEFT, 3*RIGHT).set_color(GREEN)
+            line3 = Line(3*LEFT, 3*RIGHT).shift(DOWN).set_color(BLUE)
+
+            dot1 = Dot().move_to(line1.get_left())
+            dot2 = Dot().move_to(line2.get_left())
+            dot3 = Dot().move_to(line3.get_left())
+
+            label1 = Tex("Ease In").next_to(line1, RIGHT)
+            label2 = Tex("Ease out").next_to(line2, RIGHT)
+            label3 = Tex("Ease In Out").next_to(line3, RIGHT)
+
+            self.play(
+                FadeIn(VGroup(line1, line2, line3), 
+                FadeIn(VGroup(dot1, dot2, dot3))),
+                Write(VGroup(label1, label2, label3)),
+            )
+            self.play(
+                MoveAlongPath(dot1, line1, rate_func=ease_in_sine),
+                MoveAlongPath(dot2, line2, rate_func=ease_out_sine),
+                MoveAlongPath(dot3, line3, rate_func=ease_in_out_sine),
+                run_time=7
+            )
+            self.wait()
+"""
 
 
 __all__ = [
