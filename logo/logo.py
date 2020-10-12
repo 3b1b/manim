2d529bf21cc0e936924d012043fa9688c8e220f1
MysaaJava <samyavrillon@netcourrier.com>
2020-10-12 20:39:52 +0200

    Taking leotrs's review in account

--- logo/logo.py
+++ logo/logo.py
@@ -98,9 +98,7 @@ class Thumbnail(GraphScene):
         graph_dot_p2.move_to(get_graph_point(input_tracker_p2))
 
         #
-        self.play(
-            ShowCreation(graph),
-        )
+        self.play(ShowCreation(graph))
         # Animacion del punto a
         self.add_foreground_mobject(graph_dot_p1)
         self.add_foreground_mobject(graph_dot_p2)
