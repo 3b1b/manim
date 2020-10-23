282cf3cd0eb52e54fbadeff28a814e910eee0729
Naveen M K <naveen@syrusdark.website>
2020-10-23 13:13:49 +0530

    Add Rich traceback (#573)
    * feat: add rich tracebacks
    
    * clean
    
    * fix import
    
    * fix lock file

--- manim/config/logger.py
+++ manim/config/logger.py
@@ -10,12 +10,12 @@ This library uses rich for coloured log outputs.
 __all__ = ["logger", "console"]
 
 
-import configparser
 import logging
 
 from rich.console import Console
 from rich.logging import RichHandler
 from rich.theme import Theme
+from rich.traceback import install
 from rich import print as printf
 from rich import errors, color
 import json
@@ -114,7 +114,7 @@ def set_file_logger(log_file_path):
 logger = logging.getLogger("manim")
 # The console is set to None as it will be changed by set_rich_logger.
 console = None
-
+install()
 # TODO : This is only temporary to keep the terminal output clean when working with ImageMobject and matplotlib plots
 logging.getLogger("PIL").setLevel(logging.INFO)
 logging.getLogger("matplotlib").setLevel(logging.INFO)
