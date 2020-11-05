fe9d2b5f37cf048adb73a4f8d60431c870cbe711
kolibril13 <jan-hendrik.mueller@gmx.net>
2020-11-05 19:07:48 +0100

    use circle instead

--- tests/test_graphical_units/test_threed.py
+++ tests/test_graphical_units/test_threed.py
@@ -38,12 +38,10 @@ class FixedInFrameMObjectTest(ThreeDScene):
     def construct(self):
         axes = ThreeDAxes()
         self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES)
-        text3d = Text("This is a 3D text")
-        self.add_fixed_in_frame_mobjects(text3d)
-        text3d.to_corner(UL)
+        circ = Circle()
+        self.add_fixed_in_frame_mobjects(circ)
+        circ.to_corner(UL)
         self.add(axes)
-        self.wait()
-
 
 MODULE_NAME = "threed"
 
