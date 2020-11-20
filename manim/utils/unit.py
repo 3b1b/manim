44c65b43dc3578d3bfbb77bf3d1ebb77454d5a8c
leotrs <leo@leotrs.com>
2020-11-19 21:32:08 -0500

    blaaaaaaack

--- manim/utils/unit.py
+++ manim/utils/unit.py
@@ -19,4 +19,4 @@ def pct(val, axis):
     if np.array_equal(axis, constants.Y_AXIS):
         return val / 100 * config.frame_height
     if np.array_equal(axis, constants.Z_AXIS):
-        raise NotImplementedError('length of Z axis is undefined...')
+        raise NotImplementedError("length of Z axis is undefined...")
