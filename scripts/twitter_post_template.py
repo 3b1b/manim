19d0d51532148ce45aaffa4444b0da3099bff934
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2020-12-01 14:31:34 +0100

    Added a template for twitter posts (#800)
    * added twitter template
    
    * Update scripts/twitter_post_template.py
    
    Co-authored-by: Benjamin Hackl <devel@benjamin-hackl.at>
    
    Co-authored-by: Benjamin Hackl <devel@benjamin-hackl.at>

---
+++ scripts/twitter_post_template.py
@@ -0,0 +1,17 @@
+from manim import *
+import pkg_resources
+
+version_num = pkg_resources.get_distribution("manimce").version
+
+
+class TwitterScene(Scene):
+    def construct(self):
+        self.camera.background_color = "#ece6e2"
+        version = Tex(f"v{version_num}").to_corner(UR).set_color(BLACK)
+        self.add(version)
+        ## add twitter scene content here
+
+        banner = ManimBanner(dark_theme=False).scale(0.3).to_corner(DR)
+        self.play(FadeIn(banner))
+        self.play(banner.expand())
+        self.play(FadeOut(banner))
