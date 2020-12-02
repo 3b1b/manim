9d61aac8b7ff87685635eceed900347f25e069d5
leotrs <leo@leotrs.com>
2020-12-01 23:03:30 -0500

    change interface of units

--- manim/utils/unit.py
+++ manim/utils/unit.py
@@ -5,18 +5,33 @@ import numpy as np
 from .. import constants, config
 
 
-def m(val):
-    return val
+__all__ = ['Pixels', 'Degrees', 'Munits', 'Percent']
 
 
-def px(val):
-    return val * config.frame_width / config.pixel_width
+class _PixelUnits:
+    def __mul__(self, val):
+        return val * config.frame_width / config.pixel_width
+
+    def __rmul__(self, val):
+        return val * config.frame_width / config.pixel_width
 
 
-def pct(val, axis):
-    if np.array_equal(axis, constants.X_AXIS):
-        return val / 100 * config.frame_width
-    if np.array_equal(axis, constants.Y_AXIS):
-        return val / 100 * config.frame_height
-    if np.array_equal(axis, constants.Z_AXIS):
-        raise NotImplementedError("length of Z axis is undefined...")
+class Percent:
+    def __init__(self, axis):
+        if np.array_equal(axis, constants.X_AXIS):
+            self.length = config.frame_width
+        if np.array_equal(axis, constants.Y_AXIS):
+            self.length = config.frame_height
+        if np.array_equal(axis, constants.Z_AXIS):
+            raise NotImplementedError("length of Z axis is undefined")
+
+    def __mul__(self, val):
+        return val / 100 * self.length
+
+    def __rmul__(self, val):
+        return val / 100 * self.length
+
+
+Pixels = _PixelUnits()
+Degrees = constants.PI / 180
+Munits = 1
