343253b78708bfa682e8258fc1b2ae6af375c915
Grant Sanderson <grant@3blue1brown.com>
2021-01-02 11:12:43 -0800

    Remove livestream related arguments

--- manimlib/config.py
+++ manimlib/config.py
@@ -128,34 +128,7 @@ def parse_cli():
             "--tex_dir",
             help="directory to write tex",
         )
-
-        # For live streaming
-        module_location.add_argument(
-            "--livestream",
-            action="store_true",
-            help="Run in streaming mode",
-        )
-        parser.add_argument(
-            "--to-twitch",
-            action="store_true",
-            help="Stream to twitch",
-        )
-        parser.add_argument(
-            "--with-key",
-            dest="twitch_key",
-            help="Stream key for twitch",
-        )
         args = parser.parse_args()
-
-        if args.file is None and not args.livestream:
-            parser.print_help()
-            sys.exit(2)
-        if args.to_twitch and not args.livestream:
-            print("You must run in streaming mode in order to stream to twitch")
-            sys.exit(2)
-        if args.to_twitch and args.twitch_key is None:
-            print("Specify the twitch stream key with --with-key")
-            sys.exit(2)
         return args
     except argparse.ArgumentError as err:
         print(str(err))
