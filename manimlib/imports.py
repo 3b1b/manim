a6d484236c3d750d2a63a376a81ea71dcf4064bb
Grant Sanderson <grant@3blue1brown.com>
2021-01-04 14:55:55 -0800

    Add DotCloud to universal imports

--- manimlib/imports.py
+++ manimlib/imports.py
@@ -52,6 +52,7 @@ from manimlib.mobject.types.image_mobject import *
 from manimlib.mobject.types.point_cloud_mobject import *
 from manimlib.mobject.types.surface import *
 from manimlib.mobject.types.vectorized_mobject import *
+from manimlib.mobject.types.dot_cloud import *
 from manimlib.mobject.mobject_update_utils import *
 from manimlib.mobject.value_tracker import *
 from manimlib.mobject.vector_field import *
