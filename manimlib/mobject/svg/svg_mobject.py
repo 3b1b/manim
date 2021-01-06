cffe172df0745c0aad7a57e2592cc06ef45aecff
Christopher Muñoz <chmunozpardo@gmail.com>
2021-01-06 13:59:15 -0300

    Fixed an SVG problem with moveto commands after a closepath command

--- manimlib/mobject/svg/svg_mobject.py
+++ manimlib/mobject/svg/svg_mobject.py
@@ -34,8 +34,9 @@ class SVGMobject(VMobject):
         "width": None,
         # Must be filled in in a subclass, or when called
         "file_name": None,
+        "fill_colors": True,
         "unpack_groups": True,  # if False, creates a hierarchy of VGroups
-        "stroke_width": DEFAULT_STROKE_WIDTH,
+        "stroke_width": 1.0,
         "fill_opacity": 1.0,
         # "fill_color" : LIGHT_GREY,
     }
@@ -43,9 +44,30 @@ class SVGMobject(VMobject):
     def __init__(self, file_name=None, **kwargs):
         digest_config(self, kwargs)
         self.file_name = file_name or self.file_name
+        self.color_fills = []
         self.ensure_valid_file()
         VMobject.__init__(self, **kwargs)
         self.move_into_position()
+
+    def append_color(self, element):
+        if self.fill_colors:
+            fill_color = element.getAttribute("fill")
+            if fill_color:
+                if fill_color == "none":
+                    self.color_fills.append("#000000")
+                elif len(fill_color) == 4:
+                    new_color_fill = fill_color[0] + fill_color[1]*2+fill_color[2]*2+fill_color[3]*2
+                    self.color_fills.append(new_color_fill)
+                else:
+                    self.color_fills.append(fill_color)
+            else:
+                self.color_fills.append("#000000")
+
+    def paint(self):
+        print(self.color_fills)
+        print(self.submobjects)
+        for index in range(len(self.submobjects)):
+            self[index].set_color(self.color_fills[index])
 
     def ensure_valid_file(self):
         if self.file_name is None:
@@ -78,6 +100,8 @@ class SVGMobject(VMobject):
         result = []
         if not isinstance(element, minidom.Element):
             return result
+        if element.getAttribute('id'):
+                self.update_ref_to_element(element)
         if element.tagName == 'defs':
             self.update_ref_to_element(element)
         elif element.tagName == 'style':
@@ -91,15 +115,20 @@ class SVGMobject(VMobject):
             temp = element.getAttribute('d')
             if temp != '':
                 result.append(self.path_string_to_mobject(temp))
+            self.append_color(element)
         elif element.tagName == 'use':
             result += self.use_to_mobjects(element)
         elif element.tagName == 'rect':
             result.append(self.rect_to_mobject(element))
+            self.append_color(element)
         elif element.tagName == 'circle':
             result.append(self.circle_to_mobject(element))
+            self.append_color(element)
         elif element.tagName == 'ellipse':
             result.append(self.ellipse_to_mobject(element))
+            self.append_color(element)
         elif element.tagName in ['polygon', 'polyline']:
+            self.append_color(element)
             result.append(self.polygon_to_mobject(element))
         else:
             pass  # TODO
@@ -108,7 +137,6 @@ class SVGMobject(VMobject):
         self.handle_transforms(element, VGroup(*result))
         if len(result) > 1 and not self.unpack_groups:
             result = [VGroup(*result)]
-
         return result
 
     def g_to_mobjects(self, g_element):
@@ -173,7 +201,7 @@ class SVGMobject(VMobject):
         stroke_color = rect_element.getAttribute("stroke")
         stroke_width = rect_element.getAttribute("stroke-width")
         corner_radius = rect_element.getAttribute("rx")
-
+        opacity = 1
         # input preprocessing
         if fill_color in ["", "none", "#FFF", "#FFFFFF"] or Color(fill_color) == Color(WHITE):
             opacity = 0
@@ -320,6 +348,8 @@ class SVGMobject(VMobject):
 class VMobjectFromSVGPathstring(VMobject):
     def __init__(self, path_string, **kwargs):
         digest_locals(self)
+        self.start_point = [0.0, 0.0, 0.0]
+        self.z_state = False
         VMobject.__init__(self, **kwargs)
 
     def get_path_commands(self):
@@ -359,17 +389,25 @@ class VMobjectFromSVGPathstring(VMobject):
         points = self.points
         new_points = self.string_to_points(coord_string)
 
+        temp_points = new_points.copy()
         if isLower and len(points) > 0:
             new_points += points[-1]
 
         if command == "M":  # moveto
+            if len(points) == 0:
+                self.start_point = new_points[0]
+            if self.z_state:
+                self.z_state = False
+                if isLower:
+                    new_points[0] = self.start_point + temp_points[0]
+                    self.start_point = new_points[0]
             self.start_new_path(new_points[0])
             if len(new_points) <= 1:
                 return
 
             # Draw relative line-to values.
             points = self.points
-            new_points = new_points[1:]
+            new_points = temp_points[1:]
             command = "L"
 
             for p in new_points:
@@ -383,21 +421,37 @@ class VMobjectFromSVGPathstring(VMobject):
         elif command in ["L", "H", "V"]:  # lineto
             if command == "H":
                 new_points[0, 1] = points[-1, 1]
+                self.add_line_to(new_points[0])
             elif command == "V":
                 if isLower:
                     new_points[0, 0] -= points[-1, 0]
                     new_points[0, 0] += points[-1, 1]
                 new_points[0, 1] = new_points[0, 0]
                 new_points[0, 0] = points[-1, 0]
-            self.add_line_to(new_points[0])
+                self.add_line_to(new_points[0])
+            elif command == "L":
+                diff = 0
+                for i in range(len(new_points)):
+                    if isLower:
+                        diff += temp_points[i]
+                        self.add_line_to(points[-1] + diff)
+                    else:
+                        self.add_line_to(new_points[i])
             return
 
         if command == "C":  # curveto
             pass  # Yay! No action required
         elif command in ["S", "T"]:  # smooth curveto
-            self.add_smooth_curve_to(*new_points)
+            self.add_smooth_curve_to(*new_points[0:2])
             # handle1 = points[-1] + (points[-1] - points[-2])
             # new_points = np.append([handle1], new_points, axis=0)
+            if len(new_points) > 2:
+            # Add subsequent offset points relatively.
+                for i in range(2, len(new_points), 2):
+                    if isLower:
+                        new_points[i:i + 2] -= points[-1]
+                        new_points[i:i + 2] += new_points[i - 1]
+                    self.add_smooth_curve_to(*new_points[i:i+2])
             return
         elif command == "Q":  # quadratic Bezier curve
             # TODO, this is a suboptimal approximation
@@ -405,6 +459,9 @@ class VMobjectFromSVGPathstring(VMobject):
         elif command == "A":  # elliptical Arc
             previous_point = self.points[-1]
             arc_parameters = string_to_numbers(coord_string)
+            if isLower:
+                arc_parameters[5] = arc_parameters[5] + previous_point[0]
+                arc_parameters[6] = arc_parameters[6] + previous_point[1]
             curves = arc_to_bezier(
                 px = previous_point[0],
                 py = previous_point[1],
@@ -420,6 +477,8 @@ class VMobjectFromSVGPathstring(VMobject):
                 self.add_cubic_bezier_curve_to(*curve)
             return
         elif command == "Z":  # closepath
+            self.z_state = True
+            self.add_line_to(self.start_point)
             return
 
         # Add first three points
