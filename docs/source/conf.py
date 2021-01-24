ddcfd01fa3ba53240f5feb9091bc11064520805f
Tony031218 <975062472@qq.com>
2021-01-24 16:03:18 +0800

    add manimlib to path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,26 +1,14 @@
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))
 
 
 project = 'manim'
-copyright = '2021, TonyCrane'
+copyright = '- This document has been placed in the public domain.'
 author = 'TonyCrane'
 
 release = '0.1'
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -40,5 +40,4 @@ Indices and tables
 ==================
 
 * :ref:`genindex`
-* :ref:`modindex`
 * :ref:`search`
