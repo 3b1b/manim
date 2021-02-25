8fcb0698083233c18fffc3e0aadc4cfc1b05f20e
Grant Sanderson <grant@3blue1brown.com>
2021-02-25 08:45:49 -0800

    Added some methods to coordinate system to access all axes ranges

--- manimlib/mobject/coordinate_systems.py
+++ manimlib/mobject/coordinate_systems.py
@@ -46,7 +46,13 @@ class CoordinateSystem():
         """Abbreviation for point_to_coords"""
         return self.point_to_coords(point)
 
+    def get_origin(self):
+        return self.c2p(*[0] * self.dimension)
+
     def get_axes(self):
+        raise Exception("Not implemented")
+
+    def get_all_ranges(self):
         raise Exception("Not implemented")
 
     def get_axis(self, index):
@@ -319,6 +325,9 @@ class Axes(VGroup, CoordinateSystem):
     def get_axes(self):
         return self.axes
 
+    def get_all_ranges(self):
+        return [self.x_range, self.y_range]
+
     def add_coordinate_labels(self,
                               x_values=None,
                               y_values=None,
@@ -346,9 +355,11 @@ class ThreeDAxes(Axes):
 
     def __init__(self, x_range=None, y_range=None, z_range=None, **kwargs):
         Axes.__init__(self, x_range, y_range, **kwargs)
+        if z_range is not None:
+            self.z_range[:len(z_range)] = z_range
 
         z_axis = self.create_axis(
-            z_range or self.z_range,
+            self.z_range,
             self.z_axis_config,
             self.depth,
         )
@@ -364,6 +375,9 @@ class ThreeDAxes(Axes):
 
         for axis in self.axes:
             axis.insert_n_curves(self.num_axis_pieces - 1)
+
+    def get_all_ranges(self):
+        return [self.x_range, self.y_range, self.z_range]
 
 
 class NumberPlane(Axes):
