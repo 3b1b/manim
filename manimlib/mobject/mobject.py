1197a15bbb811a33780ba47cb0931a84f0668a0e
Sahil Makhijani <sahil.makhijani@gmail.com>
2021-06-17 11:29:49 +0530

    Remove Unused "listen_to_events" CONFIG of Mobject

--- manimlib/mobject/mobject.py
+++ manimlib/mobject/mobject.py
@@ -56,9 +56,7 @@ class Mobject(object):
         # Must match in attributes of vert shader
         "shader_dtype": [
             ('point', np.float32, (3,)),
-        ],
-        # Event listener
-        "listen_to_events": False
+        ]
     }
 
     def __init__(self, **kwargs):
