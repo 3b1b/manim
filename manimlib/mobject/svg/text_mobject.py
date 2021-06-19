663c57ba74a55be103a3c1cec0c0dffe04db77d7
TonyCrane <975062472@qq.com>
2021-06-19 19:33:53 +0800

    a little fix to TEXT_MOB_SCALE_FACTOR

--- manimlib/mobject/svg/text_mobject.py
+++ manimlib/mobject/svg/text_mobject.py
@@ -18,7 +18,7 @@ from manimlib.utils.directories import get_downloads_dir, get_text_dir
 from manimpango import PangoUtils
 from manimpango import TextSetting
 
-TEXT_MOB_SCALE_FACTOR = 1/100
+TEXT_MOB_SCALE_FACTOR = 0.0076
 DEFAULT_LINE_SPACING_SCALE = 0.3
 
 class Text(SVGMobject):
