b489490f41840ca2e7f02cd23f1b532d221c5959
DangGiaChi <danggiachi@gmail.com>
2021-12-17 07:14:37 +0700

    Fixed things as suggestions

--- manimlib/mobject/probability.py
+++ manimlib/mobject/probability.py
@@ -8,7 +8,6 @@ from manimlib.mobject.svg.tex_mobject import TexText
 from manimlib.mobject.types.vectorized_mobject import VGroup
 from manimlib.utils.color import color_gradient
 from manimlib.utils.iterables import listify
-from numpy import ndarray
 
 EPSILON = 0.0001
 
@@ -152,7 +151,7 @@ class BarChart(VGroup):
         "n_ticks": 4,
         "include_x_ticks": False,
         "tick_width": 0.2,
-        "tick_width_x": 0.15,
+        "tick_height": 0.15,
         "label_y_axis": True,
         "y_axis_label_height": 0.25,
         "max_value": 1,
@@ -187,15 +186,14 @@ class BarChart(VGroup):
         y_axis.add(ticks)
 
         if self.include_x_ticks == True:
-            ticks1 = VGroup()
             widths = np.linspace(0, self.width, self.n_ticks_x + 1)
             label_values = np.linspace(0, len(self.bar_names), self.n_ticks_x + 1)
             for x, value in zip(widths, label_values):
-                tick1 = Line(UP*0.05, DOWN*0.05)
-                tick1.set_width(self.tick_width_x)
+                tick1 = Line(UP, DOWN)
+                tick1.set_height(self.tick_height)
                 tick1.move_to(x * RIGHT)
-                ticks.add(tick1)
-            x_axis.add(ticks1)
+                ticks.add(tick)
+            x_axis.add(ticks)
 
         self.add(x_axis, y_axis)
         self.x_axis, self.y_axis = x_axis, y_axis
