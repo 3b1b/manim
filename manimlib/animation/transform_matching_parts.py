d87142e04811125fc00bfe6df5421c4cb785948f
YishiMichael <50232075+YishiMichael@users.noreply.github.com>
2022-01-03 13:49:51 +0800

    Implement `TransformMatchingMTex`

--- manimlib/animation/transform_matching_parts.py
+++ manimlib/animation/transform_matching_parts.py
@@ -1,3 +1,4 @@
+import itertools as it
 import numpy as np
 
 from manimlib.animation.composition import AnimationGroup
@@ -8,9 +9,11 @@ from manimlib.animation.transform import Transform
 
 from manimlib.mobject.mobject import Mobject
 from manimlib.mobject.mobject import Group
+from manimlib.mobject.svg.mtex_mobject import MTex
 from manimlib.mobject.types.vectorized_mobject import VGroup
 from manimlib.mobject.types.vectorized_mobject import VMobject
 from manimlib.utils.config_ops import digest_config
+from manimlib.utils.iterables import remove_list_redundancies
 
 
 class TransformMatchingParts(AnimationGroup):
@@ -139,3 +142,98 @@ class TransformMatchingTex(TransformMatchingParts):
     @staticmethod
     def get_mobject_key(mobject):
         return mobject.get_tex()
+
+
+class TransformMatchingMTex(AnimationGroup):
+    CONFIG = {
+        "mismatches_anim_class": None,
+        "key_map": dict(),
+    }
+
+    def __init__(self, mobject, target_mobject, **kwargs):
+        digest_config(self, kwargs)
+        assert isinstance(mobject, MTex)
+        assert isinstance(target_mobject, MTex)
+        anims = []
+
+        def get_submobjects_from_keys(mob, keys):
+            if not isinstance(keys, tuple):
+                keys = (keys,)
+            submobs_list = []
+            for key in keys:
+                if isinstance(key, int):
+                    submobs_list.append(mob[key])
+                elif isinstance(key, range):
+                    submobs_list.extend([mob[index] for index in key])
+                elif isinstance(key, str):
+                    submobs_list.extend(it.chain(*mob.get_parts_by_tex(key)))
+                else:
+                    raise ValueError(f"Unexpected key: \"{key}\"")
+            return VGroup(*remove_list_redundancies(submobs_list))
+
+        rest_src_submobs = mobject.submobjects
+        rest_tar_submobs = target_mobject.submobjects
+        for src_keys, tar_keys in self.key_map.items():
+            src_submobs = VGroup(*filter(
+                lambda m: m in rest_src_submobs,
+                get_submobjects_from_keys(mobject, src_keys)
+            ))
+            rest_src_submobs = list(filter(
+                lambda m: m not in src_submobs,
+                rest_src_submobs
+            ))
+            tar_submobs = VGroup(*filter(
+                lambda m: m in rest_tar_submobs,
+                get_submobjects_from_keys(target_mobject, tar_keys)
+            ))
+            rest_tar_submobs = list(filter(
+                lambda m: m not in tar_submobs,
+                rest_tar_submobs
+            ))
+            anims.append(FadeTransformPieces(src_submobs, tar_submobs, **kwargs))
+
+        def get_shape_map_from_submobjects(submobs):
+            shape_map = {}
+            for sm in submobs:
+                key = sm.get_tex()
+                if key not in shape_map:
+                    shape_map[key] = VGroup()
+                shape_map[key].add(sm)
+            return shape_map
+
+        source_map = get_shape_map_from_submobjects(rest_src_submobs)
+        target_map = get_shape_map_from_submobjects(rest_tar_submobs)
+
+        for key in set(source_map).intersection(target_map):
+            anims.append(Transform(source_map[key], target_map[key], **kwargs))
+
+        fade_source = VGroup(*[
+            source_map[key]
+            for key in set(source_map).difference(target_map)
+        ])
+        fade_target = VGroup(*[
+            target_map[key]
+            for key in set(target_map).difference(source_map)
+        ])
+
+        if self.mismatches_anim_class is not None:
+            anims.append(self.mismatches_anim_class(fade_source, fade_target, **kwargs))
+        else:
+            anims.append(FadeOutToPoint(
+                fade_source, target_mobject.get_center(), **kwargs
+            ))
+            anims.append(FadeInFromPoint(
+                fade_target.copy(), mobject.get_center(), **kwargs
+            ))
+
+        super().__init__(*anims)
+
+        self.to_remove = mobject
+        self.to_add = target_mobject
+
+    def clean_up_from_scene(self, scene):
+        for anim in self.animations:
+            anim.update(0)
+        scene.remove(self.mobject)
+        scene.remove(self.to_remove)
+        scene.add(self.to_add)
