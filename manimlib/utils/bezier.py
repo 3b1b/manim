eb245f6fdbeeb980b35898acadb0a071837664dc
Grant Sanderson <grant@3blue1brown.com>
2022-01-25 08:57:43 -0800

    Use generator instead of list in bezier

--- manimlib/utils/bezier.py
+++ manimlib/utils/bezier.py
@@ -14,10 +14,10 @@ def bezier(points):
     n = len(points) - 1
 
     def result(t):
-        return sum([
+        return sum(
             ((1 - t)**(n - k)) * (t**k) * choose(n, k) * point
             for k, point in enumerate(points)
-        ])
+        )
 
     return result
 
