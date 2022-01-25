f73f2984a7b19b6c9954c4b67a353e7df916098f
YishiMichael <50232075+YishiMichael@users.noreply.github.com>
2022-01-25 23:22:12 +0800

    Remove debugging opt

--- manimlib/mathjax/index.js
+++ manimlib/mathjax/index.js
@@ -2,8 +2,4 @@ const fs = require('fs')
 const tex2svg = require('./tex2svg.js')
 
 let { argv } = process
-if (argv[2] === '-v') {
-    console.log(process.argv)
-    argv = argv.slice(1)
-}
 tex2svg(argv[3]).then(svg => fs.writeFileSync(argv[2], svg))
