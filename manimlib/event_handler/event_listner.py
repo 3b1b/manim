62cab9feaf050444722ac551ae7ba6fd48e3bcb4
TonyCrane <tonycrane@foxmail.com>
2022-02-14 21:25:46 +0800

    chore: re-add type hint for EventListener

--- manimlib/event_handler/event_listner.py
+++ manimlib/event_handler/event_listner.py
@@ -1,5 +1,18 @@
+from __future__ import annotations
+
+from typing import Callable, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from manimlib.mobject.mobject import Mobject
+    from manimlib.event_handler.event_type import EventType
+
 class EventListner(object):
-    def __init__(self, mobject, event_type, event_callback):
+    def __init__(
+        self,
+        mobject: Mobject,
+        event_type: EventType,
+        event_callback: Callable[[Mobject, dict[str]]]
+    ):
         self.mobject = mobject
         self.event_type = event_type
         self.callback = event_callback
