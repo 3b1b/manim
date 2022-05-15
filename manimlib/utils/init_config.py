fb3cf308dfbf521d1592824bf15749f410b715f0
Grant Sanderson <grant@3blue1brown.com>
2022-05-14 17:42:07 -0700

    Make sure init_customization matches default_config.yml

--- manimlib/utils/init_config.py
+++ manimlib/utils/init_config.py
@@ -57,25 +57,14 @@ def init_customization() -> None:
         "window_monitor": 0,
         "full_screen": False,
         "break_into_partial_movies": False,
-        "camera_qualities": {
-            "low": {
-                "resolution": "854x480",
-                "frame_rate": 15,
-            },
-            "medium": {
-                "resolution": "1280x720",
-                "frame_rate": 30,
-            },
-            "high": {
-                "resolution": "1920x1080",
-                "frame_rate": 60,
-            },
-            "ultra_high": {
-                "resolution": "3840x2160",
-                "frame_rate": 60,
-            },
-            "default_quality": "",
-        }
+        "camera_resolutions": {
+            "low": "854x480",
+            "medium": "1280x720",
+            "high": "1920x1080",
+            "4k": "3840x2160",
+            "default_resolution": "high",
+        },
+        "frame_rate": 30,
     }
 
     console = Console()
@@ -83,7 +72,7 @@ def init_customization() -> None:
     # print("Initialize configuration")
     try:
         scope = Prompt.ask(
-            "  Select the scope of the configuration", 
+            "  Select the scope of the configuration",
             choices=["global", "local"],
             default="local"
         )
