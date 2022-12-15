5a309d41b7d6bbd1822738261f72cbd60eb1ebb2
Grant Sanderson <grant@3blue1brown.com>
2022-12-15 11:33:53 -0800

    Undo potentially undefined typing

--- manimlib/constants.py
+++ manimlib/constants.py
@@ -7,6 +7,7 @@ if TYPE_CHECKING:
     from colour import Color
 
     from typing import List
+
     # Abbreviations for a common types
     ManimColor = Union[str, Color, None]
     np_vector = np.ndarray[int, np.dtype[np.float64]]
@@ -35,27 +36,27 @@ DEFAULT_MOBJECT_TO_MOBJECT_BUFFER: float = MED_SMALL_BUFF
 DEFAULT_WAIT_TIME: float = 1.0
 
 
-ORIGIN: np_vector = np.array([0., 0., 0.])
-UP: np_vector = np.array([0., 1., 0.])
-DOWN: np_vector = np.array([0., -1., 0.])
-RIGHT: np_vector = np.array([1., 0., 0.])
-LEFT: np_vector = np.array([-1., 0., 0.])
-IN: np_vector = np.array([0., 0., -1.])
-OUT: np_vector = np.array([0., 0., 1.])
-X_AXIS: np_vector = np.array([1., 0., 0.])
-Y_AXIS: np_vector = np.array([0., 1., 0.])
-Z_AXIS: np_vector = np.array([0., 0., 1.])
+ORIGIN = np.array([0., 0., 0.])
+UP = np.array([0., 1., 0.])
+DOWN = np.array([0., -1., 0.])
+RIGHT = np.array([1., 0., 0.])
+LEFT = np.array([-1., 0., 0.])
+IN = np.array([0., 0., -1.])
+OUT = np.array([0., 0., 1.])
+X_AXIS = np.array([1., 0., 0.])
+Y_AXIS = np.array([0., 1., 0.])
+Z_AXIS = np.array([0., 0., 1.])
 
 # Useful abbreviations for diagonals
-UL: np_vector = UP + LEFT
-UR: np_vector = UP + RIGHT
-DL: np_vector = DOWN + LEFT
-DR: np_vector = DOWN + RIGHT
+UL = UP + LEFT
+UR = UP + RIGHT
+DL = DOWN + LEFT
+DR = DOWN + RIGHT
 
-TOP: np_vector = FRAME_Y_RADIUS * UP
-BOTTOM: np_vector = FRAME_Y_RADIUS * DOWN
-LEFT_SIDE: np_vector = FRAME_X_RADIUS * LEFT
-RIGHT_SIDE: np_vector = FRAME_X_RADIUS * RIGHT
+TOP = FRAME_Y_RADIUS * UP
+BOTTOM = FRAME_Y_RADIUS * DOWN
+LEFT_SIDE = FRAME_X_RADIUS * LEFT
+RIGHT_SIDE = FRAME_X_RADIUS * RIGHT
 
 PI: float = np.pi
 TAU: float = 2 * PI
@@ -86,7 +87,7 @@ CTRL_SYMBOL: int = 65508
 SHIFT_SYMBOL: int = 65505
 COMMAND_SYMBOL: int = 65517
 DELETE_SYMBOL: int = 65288
-ARROW_SYMBOLS: List[int] = list(range(65361, 65365))
+ARROW_SYMBOLS: list[int] = list(range(65361, 65365))
 
 SHIFT_MODIFIER: int = 1
 CTRL_MODIFIER: int = 2
@@ -94,62 +95,62 @@ COMMAND_MODIFIER: int = 64
 
 # Colors
 
-BLUE_E: ManimColor = "#1C758A"
-BLUE_D: ManimColor = "#29ABCA"
-BLUE_C: ManimColor = "#58C4DD"
-BLUE_B: ManimColor = "#9CDCEB"
-BLUE_A: ManimColor = "#C7E9F1"
-TEAL_E: ManimColor = "#49A88F"
-TEAL_D: ManimColor = "#55C1A7"
-TEAL_C: ManimColor = "#5CD0B3"
-TEAL_B: ManimColor = "#76DDC0"
-TEAL_A: ManimColor = "#ACEAD7"
-GREEN_E: ManimColor = "#699C52"
-GREEN_D: ManimColor = "#77B05D"
-GREEN_C: ManimColor = "#83C167"
-GREEN_B: ManimColor = "#A6CF8C"
-GREEN_A: ManimColor = "#C9E2AE"
-YELLOW_E: ManimColor = "#E8C11C"
-YELLOW_D: ManimColor = "#F4D345"
-YELLOW_C: ManimColor = "#FFFF00"
-YELLOW_B: ManimColor = "#FFEA94"
-YELLOW_A: ManimColor = "#FFF1B6"
-GOLD_E: ManimColor = "#C78D46"
-GOLD_D: ManimColor = "#E1A158"
-GOLD_C: ManimColor = "#F0AC5F"
-GOLD_B: ManimColor = "#F9B775"
-GOLD_A: ManimColor = "#F7C797"
-RED_E: ManimColor = "#CF5044"
-RED_D: ManimColor = "#E65A4C"
-RED_C: ManimColor = "#FC6255"
-RED_B: ManimColor = "#FF8080"
-RED_A: ManimColor = "#F7A1A3"
-MAROON_E: ManimColor = "#94424F"
-MAROON_D: ManimColor = "#A24D61"
-MAROON_C: ManimColor = "#C55F73"
-MAROON_B: ManimColor = "#EC92AB"
-MAROON_A: ManimColor = "#ECABC1"
-PURPLE_E: ManimColor = "#644172"
-PURPLE_D: ManimColor = "#715582"
-PURPLE_C: ManimColor = "#9A72AC"
-PURPLE_B: ManimColor = "#B189C6"
-PURPLE_A: ManimColor = "#CAA3E8"
-GREY_E: ManimColor = "#222222"
-GREY_D: ManimColor = "#444444"
-GREY_C: ManimColor = "#888888"
-GREY_B: ManimColor = "#BBBBBB"
-GREY_A: ManimColor = "#DDDDDD"
-WHITE: ManimColor = "#FFFFFF"
-BLACK: ManimColor = "#000000"
-GREY_BROWN: ManimColor = "#736357"
-DARK_BROWN: ManimColor = "#8B4513"
-LIGHT_BROWN: ManimColor = "#CD853F"
-PINK: ManimColor = "#D147BD"
-LIGHT_PINK: ManimColor = "#DC75CD"
-GREEN_SCREEN: ManimColor = "#00FF00"
-ORANGE: ManimColor = "#FF862F"
+BLUE_E = "#1C758A"
+BLUE_D = "#29ABCA"
+BLUE_C = "#58C4DD"
+BLUE_B = "#9CDCEB"
+BLUE_A = "#C7E9F1"
+TEAL_E = "#49A88F"
+TEAL_D = "#55C1A7"
+TEAL_C = "#5CD0B3"
+TEAL_B = "#76DDC0"
+TEAL_A = "#ACEAD7"
+GREEN_E = "#699C52"
+GREEN_D = "#77B05D"
+GREEN_C = "#83C167"
+GREEN_B = "#A6CF8C"
+GREEN_A = "#C9E2AE"
+YELLOW_E = "#E8C11C"
+YELLOW_D = "#F4D345"
+YELLOW_C = "#FFFF00"
+YELLOW_B = "#FFEA94"
+YELLOW_A = "#FFF1B6"
+GOLD_E = "#C78D46"
+GOLD_D = "#E1A158"
+GOLD_C = "#F0AC5F"
+GOLD_B = "#F9B775"
+GOLD_A = "#F7C797"
+RED_E = "#CF5044"
+RED_D = "#E65A4C"
+RED_C = "#FC6255"
+RED_B = "#FF8080"
+RED_A = "#F7A1A3"
+MAROON_E = "#94424F"
+MAROON_D = "#A24D61"
+MAROON_C = "#C55F73"
+MAROON_B = "#EC92AB"
+MAROON_A = "#ECABC1"
+PURPLE_E = "#644172"
+PURPLE_D = "#715582"
+PURPLE_C = "#9A72AC"
+PURPLE_B = "#B189C6"
+PURPLE_A = "#CAA3E8"
+GREY_E = "#222222"
+GREY_D = "#444444"
+GREY_C = "#888888"
+GREY_B = "#BBBBBB"
+GREY_A = "#DDDDDD"
+WHITE = "#FFFFFF"
+BLACK = "#000000"
+GREY_BROWN = "#736357"
+DARK_BROWN = "#8B4513"
+LIGHT_BROWN = "#CD853F"
+PINK = "#D147BD"
+LIGHT_PINK = "#DC75CD"
+GREEN_SCREEN = "#00FF00"
+ORANGE = "#FF862F"
 
-MANIM_COLORS: List[ManimColor] = [
+MANIM_COLORS = [
     BLACK, GREY_E, GREY_D, GREY_C, GREY_B, GREY_A, WHITE,
     BLUE_E, BLUE_D, BLUE_C, BLUE_B, BLUE_A,
     TEAL_E, TEAL_D, TEAL_C, TEAL_B, TEAL_A,
@@ -164,14 +165,14 @@ MANIM_COLORS: List[ManimColor] = [
 ]
 
 # Abbreviated names for the "median" colors
-BLUE: ManimColor = BLUE_C
-TEAL: ManimColor = TEAL_C
-GREEN: ManimColor = GREEN_C
-YELLOW: ManimColor = YELLOW_C
-GOLD: ManimColor = GOLD_C
-RED: ManimColor = RED_C
-MAROON: ManimColor = MAROON_C
-PURPLE: ManimColor = PURPLE_C
-GREY: ManimColor = GREY_C
+BLUE = BLUE_C
+TEAL = TEAL_C
+GREEN = GREEN_C
+YELLOW = YELLOW_C
+GOLD = GOLD_C
+RED = RED_C
+MAROON = MAROON_C
+PURPLE = PURPLE_C
+GREY = GREY_C
 
-COLORMAP_3B1B: List[ManimColor] = [BLUE_E, GREEN, YELLOW, RED]
+COLORMAP_3B1B = [BLUE_E, GREEN, YELLOW, RED]
