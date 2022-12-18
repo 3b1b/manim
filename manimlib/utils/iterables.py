fd20ead11b20b1ab7b907a52a9574ff0d7c10c59
Grant Sanderson <grant@3blue1brown.com>
2022-12-17 21:51:01 -0800

    Update output type for make_even

--- manimlib/utils/iterables.py
+++ manimlib/utils/iterables.py
@@ -114,7 +114,7 @@ def resize_with_interpolation(nparray: np.ndarray, length: int) -> np.ndarray:
 def make_even(
     iterable_1: Sequence[T],
     iterable_2: Sequence[S]
-) -> tuple[list[T], list[S]]:
+) -> tuple[Sequence[T], Sequence[S]]:
     len1 = len(iterable_1)
     len2 = len(iterable_2)
     if len1 == len2:
