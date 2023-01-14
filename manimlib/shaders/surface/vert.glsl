1c1325ff8d9d527911ca4ce411dd4ed12374f8d2
Grant Sanderson <grant@3blue1brown.com>
2023-01-13 21:04:36 -0800

    More coloring work of Surface to vert shader

--- manimlib/shaders/surface/vert.glsl
+++ manimlib/shaders/surface/vert.glsl
@@ -2,6 +2,12 @@
 
 #INSERT camera_uniform_declarations.glsl
 
+uniform vec3 light_source_position;
+uniform vec3 camera_position;
+uniform float reflectiveness;
+uniform float gloss;
+uniform float shadow;
+
 in vec3 point;
 in vec3 du_point;
 in vec3 dv_point;
@@ -14,10 +20,22 @@ out vec4 v_color;
 #INSERT position_point_into_frame.glsl
 #INSERT get_gl_Position.glsl
 #INSERT get_rotated_surface_unit_normal_vector.glsl
+#INSERT finalize_color.glsl
 
 void main(){
     xyz_coords = position_point_into_frame(point);
     v_normal = get_rotated_surface_unit_normal_vector(point, du_point, dv_point);
     v_color = color;
     gl_Position = get_gl_Position(xyz_coords);
+
+    v_color = finalize_color(
+        color,
+        xyz_coords,
+        v_normal,
+        light_source_position,
+        camera_position,
+        reflectiveness,
+        gloss,
+        shadow
+    );
 }
