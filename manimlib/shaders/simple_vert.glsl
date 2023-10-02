8003175b9065c8ce483314d6821275e2cec0d168
sheuher <sh-tey@hotmail.com>
2023-10-02 21:39:45 +0200

    commit

--- manimlib/shaders/simple_vert.glsl
+++
@@ -1,9 +0,0 @@
-#version 330
-
-in vec3 point;
-
-#INSERT emit_gl_Position.glsl
-
-void main(){
-    emit_gl_Position(point);
-}
--- manimlib/shaders/surface/frag.glsl
+++
@@ -1,8 +0,0 @@
-#version 330
-
-in vec4 v_color;
-out vec4 frag_color;
-
-void main() {
-    frag_color = v_color;
-}
