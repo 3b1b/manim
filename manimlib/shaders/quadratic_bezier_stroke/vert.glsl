27f397e0a6c6a884fb9f7cb7193fbfe3c33feee0
Grant Sanderson <grant@3blue1brown.com>
2024-03-07 16:47:24 -0300

    Have stroke width change continuously with fixed_in_frame status

--- manimlib/shaders/quadratic_bezier_stroke/vert.glsl
+++ manimlib/shaders/quadratic_bezier_stroke/vert.glsl
@@ -21,9 +21,7 @@ const float STROKE_WIDTH_CONVERSION = 0.01;
 void main(){
     verts = point;
     v_stroke_width = STROKE_WIDTH_CONVERSION * stroke_width;
-    if(!bool(is_fixed_in_frame)){
-        v_stroke_width *= frame_scale;
-    }
+    v_stroke_width *= mix(frame_scale, 1, is_fixed_in_frame);
     v_joint_product = joint_product;
     v_color = stroke_rgba;
 }
