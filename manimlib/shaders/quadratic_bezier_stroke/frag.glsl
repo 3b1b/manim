d8edccdab4a542781477adc1d1dd6cea59d0b94a
Grant Sanderson <grant@3blue1brown.com>
2024-08-16 14:59:18 -0500

    Fix zero stroke width issue

--- manimlib/shaders/quadratic_bezier_stroke/frag.glsl
+++ manimlib/shaders/quadratic_bezier_stroke/frag.glsl
@@ -1,17 +1,18 @@
 #version 330
 
 // Value between -1 and 1
-in float scaled_signed_dist_to_curve;
-in float anti_alias_prop;
+in float dist_to_curve;
+in float half_stroke_width;
+in float half_anti_alias_width;
 in vec4 color;
 
 out vec4 frag_color;
 
 void main() {
-    if(anti_alias_prop < 0) discard;
+    if(half_stroke_width == 0) discard;
     frag_color = color;
 
     // sdf for the region around the curve we wish to color.
-    float signed_dist_to_region = abs(scaled_signed_dist_to_curve) - 1.0;
-    frag_color.a *= smoothstep(0, -anti_alias_prop, signed_dist_to_region);
+    float signed_dist_to_region = abs(dist_to_curve) - half_stroke_width;
+    frag_color.a *= smoothstep(half_anti_alias_width, -half_anti_alias_width, signed_dist_to_region);
 }
