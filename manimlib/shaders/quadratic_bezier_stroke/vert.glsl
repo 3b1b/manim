5eb5a1149975646a55ca4f0dba21331f22976e2d
Grant Sanderson <grant@3blue1brown.com>
2024-08-20 12:36:28 -0500

    Don't have absolute stroke width change with frame size (at least temporarily)

--- manimlib/shaders/quadratic_bezier_stroke/vert.glsl
+++ manimlib/shaders/quadratic_bezier_stroke/vert.glsl
@@ -20,7 +20,8 @@ const float STROKE_WIDTH_CONVERSION = 0.01;
 
 void main(){
     verts = point;
-    v_stroke_width = STROKE_WIDTH_CONVERSION * stroke_width * mix(frame_scale, 1, is_fixed_in_frame);
+    // v_stroke_width = STROKE_WIDTH_CONVERSION * stroke_width * mix(frame_scale, 1, is_fixed_in_frame);
+    v_stroke_width = STROKE_WIDTH_CONVERSION * stroke_width;
     v_joint_product = joint_product;
     v_color = stroke_rgba;
     v_vert_index = gl_VertexID;
