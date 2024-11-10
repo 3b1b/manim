8a93e90c0201849dda934083aab781377aa2e54b
Splines <dominic-plein@gmx.de>
2024-11-10 05:40:13 +0100

    Add docstrings to `ReloadManager`

--- manimlib/reload_manager.py
+++ manimlib/reload_manager.py
@@ -3,24 +3,41 @@ from typing import Any
 
 
 class ReloadManager:
+    """
+    Manages the loading and running of scenes and serves is called directly
+    from the main entry point of ManimGL.
+
+    The name "reload" comes from the fact that this class handles the
+    reinitialization of scenes when requested by the user via the `reload()`
+    command in the IPython shell.
+    """
+
     args: Any = None
     scenes: list[Any] = []
     window = None
+
+    # The line number to load the scene from when reloading
     start_at_line = None
 
     def set_new_start_at_line(self, start_at_line):
+        """
+        Sets/Updates the line number to load the scene from when reloading.
+        """
         self.start_at_line = start_at_line
 
     def run(self):
+        """
+        Runs the scenes in a loop and detects when a scene reload is requested.
+        """
         while True:
             try:
-                # This call is blocking as a scene will init an IPython shell()
+                # blocking call since a scene will init an IPython shell()
                 self.retrieve_scenes_and_run(self.start_at_line)
                 return
             except KillEmbedded:
                 # Requested via the `exit_raise` IPython runline magic
                 # by means of our scene.reload() command
-                print("KillEmbedded detected. Reloading scenes...")
+                print("Reloading...")
 
                 for scene in self.scenes:
                     scene.tear_down()
@@ -32,7 +49,7 @@ class ReloadManager:
 
     def retrieve_scenes_and_run(self, overwrite_start_at_line: int | None = None):
         """
-        Generates a configuration and runs the scenes.
+        Creates a new configuration based on the CLI args and runs the scenes.
         """
         import manimlib.config
         import manimlib.extract_scene
@@ -41,12 +58,12 @@ class ReloadManager:
         if self.args is None:
             raise RuntimeError("Fatal error: No args were passed to the ReloadManager")
         if overwrite_start_at_line is not None:
-            self.args.embed = str(overwrite_start_at_line)  # type: ignore
+            self.args.embed = str(overwrite_start_at_line)
 
         # Args to Config
         config = manimlib.config.get_configuration(self.args)
         if self.window:
-            config["existing_window"] = self.window
+            config["existing_window"] = self.window  # see scene initialization
 
         # Scenes
         self.scenes = manimlib.extract_scene.main(config)
