20a0bfaf2b726afb0abcdb84da15e7233d92d705
Splines <dominic-plein@gmx.de>
2024-12-04 12:55:49 +0100

    Clarify docstrings & move get_module method up in file

--- manimlib/module_loader.py
+++ manimlib/module_loader.py
@@ -19,19 +19,51 @@ class ModuleLoader:
     """
 
     @staticmethod
+    def get_module(file_name: str | None, is_during_reload=False) -> Module | None:
+        """
+        Imports a module from a file and returns it.
+
+        During reload (when the user calls `reload()` in the IPython shell), we
+        also track the imported modules and reload them as well (they would be
+        cached otherwise). See the reload_manager where the reload parameter is set.
+
+        Note that exec_module is called twice when reloading a module:
+        1. In exec_module_and_track_imports to track the imports
+        2. Here to actually execute the module again with the respective
+           imported modules reloaded.
+        """
+        if file_name is None:
+            return None
+        module_name = file_name.replace(os.sep, ".").replace(".py", "")
+        spec = importlib.util.spec_from_file_location(module_name, file_name)
+        module = importlib.util.module_from_spec(spec)
+
+        if is_during_reload:
+            imported_modules = ModuleLoader.exec_module_and_track_imports(spec, module)
+            reloaded_modules_tracker = set()
+            ModuleLoader.reload_modules(imported_modules, reloaded_modules_tracker)
+
+        spec.loader.exec_module(module)
+        return module
+
+    @staticmethod
     def exec_module_and_track_imports(spec, module: Module):
         """
         Executes the given module (imports it) and returns all the modules that
         are imported during its execution.
 
-        This is achieved by replacing the __import__ function with a custom one that
-        tracks the imported modules. At the end, the original __import__ function is
-        restored.
+        This is achieved by replacing the __import__ function with a custom one
+        that tracks the imported modules. At the end, the original __import__
+        built-in function is restored.
         """
         imported_modules = set()
         original_import = builtins.__import__
 
         def tracked_import(name, globals=None, locals=None, fromlist=(), level=0):
+            """
+            Custom __import__ function that does exactly the same as the original
+            one, but also tracks the imported modules by means of a set.
+            """
             result = original_import(name, globals, locals, fromlist, level)
             imported_modules.add(name)
             return result
@@ -53,6 +85,11 @@ class ModuleLoader:
         We also restrict ourselves to reloading only the modules that are not
         built-in Python modules to avoid potential issues since they were mostly
         not designed to be reloaded.
+
+        The choice of "pkg_resources" and other manually excluded modules is
+        arbitrary and based on the fact that they are not meant to be reloaded.
+        In most cases, the user will not even import them directly in their
+        script.
         """
         for mod in modules:
             if mod in reloaded_modules_tracker:
@@ -71,31 +108,3 @@ class ModuleLoader:
             importlib.reload(sys.modules[mod])
 
             reloaded_modules_tracker.add(mod)
-
-    @staticmethod
-    def get_module(file_name: str | None, is_during_reload=False) -> Module | None:
-        """
-        Imports a module from a file and returns it.
-
-        During reload (when the user calls `reload()` in the IPython shell), we
-        also track the imported modules and reload them as well (they would be
-        cached otherwise). See the reload_manager where the reload parameter is set.
-        """
-        if file_name is None:
-            return None
-        module_name = file_name.replace(os.sep, ".").replace(".py", "")
-        spec = importlib.util.spec_from_file_location(module_name, file_name)
-        module = importlib.util.module_from_spec(spec)
-
-        if is_during_reload:
-            imported_modules = ModuleLoader.exec_module_and_track_imports(spec, module)
-            reloaded_modules_tracker = set()
-            ModuleLoader.reload_modules(imported_modules, reloaded_modules_tracker)
-
-        # In the case of reloading, the following statement is executed twice:
-        # 1. to track the imports
-        # 2. to actually execute the module again with the respective
-        #    imports reloaded
-        spec.loader.exec_module(module)
-
-        return module
