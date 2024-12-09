88bae476ce3dc9d4cb8ca4349ace922d7a873311
Grant Sanderson <grant@3blue1brown.com>
2024-12-09 16:25:18 -0600

    Don't print filename that is being reloaded

--- manimlib/reload_manager.py
+++ manimlib/reload_manager.py
@@ -93,7 +93,6 @@ class ReloadManager:
                 print(" ".join([
                     "Reloading interactive session for",
                     f"\033[96m{self.args.scene_names[0]}\033[0m",
-                    f"in \033[96m{self.args.file}\033[0m",
                     f"at line \033[96m{self.args.embed}\033[0m"
                 ]))
             scene.run()
