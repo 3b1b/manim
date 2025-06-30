7839e1d6b3298057951479c37356343b734eb48d
Grant Sanderson <grant@3blue1brown.com>
2025-06-30 13:42:19 -0500

    Add remembered stroke_config to TracedPath

--- manimlib/mobject/changing.py
+++ manimlib/mobject/changing.py
@@ -101,18 +101,25 @@ class TracedPath(VMobject):
         traced_point_func: Callable[[], Vect3],
         time_traced: float = np.inf,
         time_per_anchor: float = 1.0 / 15,
-        stroke_width: float | Iterable[float] = 2.0,
         stroke_color: ManimColor = DEFAULT_MOBJECT_COLOR,
+        stroke_width: float | Iterable[float] = 2.0,
+        stroke_opacity: float = 1.0,
         **kwargs
     ):
+        self.stroke_config = dict(
+            color=stroke_color,
+            width=stroke_width,
+            opacity=stroke_opacity,
+        )
+
         super().__init__(**kwargs)
         self.traced_point_func = traced_point_func
         self.time_traced = time_traced
         self.time_per_anchor = time_per_anchor
         self.time: float = 0
-        self.traced_points: list[np.ndarray] = []
+        curr_point = traced_point_func()
+        self.traced_points: list[np.ndarray] = [curr_point.copy() for _ in range(int(time_traced / time_per_anchor))]
         self.add_updater(lambda m, dt: m.update_path(dt))
-        self.always.set_stroke(stroke_color, stroke_width)
 
     def update_path(self, dt: float) -> Self:
         if dt == 0:
@@ -136,6 +143,8 @@ class TracedPath(VMobject):
         if points:
             self.set_points_smoothly(points)
 
+        self.set_stroke(**self.stroke_config)
+
         self.time += dt
         return self
 
@@ -145,9 +154,9 @@ class TracingTail(TracedPath):
         self,
         mobject_or_func: Mobject | Callable[[], np.ndarray],
         time_traced: float = 1.0,
+        stroke_color: ManimColor = DEFAULT_MOBJECT_COLOR,
         stroke_width: float | Iterable[float] = (0, 3),
         stroke_opacity: float | Iterable[float] = (0, 1),
-        stroke_color: ManimColor = DEFAULT_MOBJECT_COLOR,
         **kwargs
     ):
         if isinstance(mobject_or_func, Mobject):
@@ -157,9 +166,8 @@ class TracingTail(TracedPath):
         super().__init__(
             func,
             time_traced=time_traced,
+            stroke_color=stroke_color,
             stroke_width=stroke_width,
             stroke_opacity=stroke_opacity,
-            stroke_color=stroke_color,
             **kwargs
         )
-        self.add_updater(lambda m: m.set_stroke(width=stroke_width, opacity=stroke_opacity))
