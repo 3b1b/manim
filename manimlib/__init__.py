88e4a03e5d051ee17defbab74e64e5f66c3fa357
Ahmer Raza <110292814+ahmrr@users.noreply.github.com>
2025-08-18 11:26:44 -0400

    Update versioning to use importlib

--- manimlib/__init__.py
+++ manimlib/__init__.py
@@ -1,6 +1,6 @@
-import pkg_resources
+from importlib.metadata import version
 
-__version__ = pkg_resources.get_distribution("manimgl").version
+__version__ = version("manimgl")
 
 from typing import TYPE_CHECKING
 
