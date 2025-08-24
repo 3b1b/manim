fe465937bcdf8861a8e0cae1ce5e30dc372ee375
Ahmed-Almahe <officialalmahui@gmail.com>
2025-08-24 17:32:07 +0300

    fix some error in boolean_ops.py file

--- manimlib/logger.py
+++ manimlib/logger.py
@@ -4,7 +4,6 @@ from rich.logging import RichHandler
 
 __all__ = ["log"]
 
-
 FORMAT = "%(message)s"
 logging.basicConfig(
     level=logging.WARNING, format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
