2085deb6eb2a3d7fd1a6fe311ff09f66c6e1a614
Aaron Gostein <aaron.gostein@gmail.com>
2026-08-14 16:30:30 -0500

    Added zoom scroll inversion option to config settings (#2507)
    * Added zoom scroll inversion option to config settings
    
    * Moved zoom scroll inversion option from window to scene settings
    
    * Added zoom scrolling inversion option in default config file

--- manimlib/scene/scene.py
+++ manimlib/scene/scene.py
@@ -75,6 +75,7 @@ class Scene(object):
         preview_while_skipping: bool = True,
         presenter_mode: bool = False,
         default_wait_time: float = 1.0,
+        invert_zoom_scroll: bool = False,
     ):
         self.skip_animations = skip_animations
         self.always_update_mobjects = always_update_mobjects
@@ -85,6 +86,7 @@ class Scene(object):
         self.preview_while_skipping = preview_while_skipping
         self.presenter_mode = presenter_mode
         self.default_wait_time = default_wait_time
+        self.invert_zoom_scroll = invert_zoom_scroll
 
         self.camera_config = merge_dicts_recursively(
             manim_config.camera,         # Global default
@@ -798,7 +800,9 @@ class Scene(object):
         if propagate_event is not None and propagate_event is False:
             return
 
-        rel_offset = y_pixel_offset / self.camera.get_pixel_height()
+        rel_offset = -y_pixel_offset / self.camera.get_pixel_height()
+        if self.invert_zoom_scroll:
+            rel_offset *= -1
         self.frame.scale(
             1 - self.scroll_sensitivity * rel_offset,
             about_point=point
